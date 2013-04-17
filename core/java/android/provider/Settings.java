7b41d33a52b9d9faf2a149ca6b6f0d81b78d4d9a
blk_jack <jcorcoran+github@gmail.com>
2013-04-17 18:46:59 -0400

    MediaScanner behavior on boot (1/3)
    Lets you enable/disable/ask (with a notification) media scanning on boot.
    Can speed up boot and reduce initial boot lag from MediaScanner
    
    Change-Id: I634ae96b1dcb493b966b4653589cd97523b63da7
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4065,6 +4065,15 @@ public final class Settings {
          * the setting value. See an example above.
          */
 
+       /**
+        * MediaScanner behavior on boot.
+        * 0 = enabled
+        * 1 = ask (notification)
+        * 2 = disabled
+        * @hide
+        */
+        public static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
+
         /**
          * Whether allowing pocket service to register sensors and dispatch informations.
          *   0 = disabled
