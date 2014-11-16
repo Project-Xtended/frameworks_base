fe534c911aa0cd0927315177e8f434d9796d6729
Mazda <mazdarider23@gmail.com>
2014-11-15 19:27:05 -0500

    android.view: reduce animation speeds Animation speed durations are a bit too high on Android 12 which makes the whole UI feel a bit less responsive, reducing these values by a small margins makes it feel snappier and a bit faster in general.
    Change-Id: If53cba16d5385532fa53d77a829866c082808926
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Karan Parashar <whyredfire@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -222,7 +222,7 @@ public class ViewConfiguration {
     /**
      * Maximum velocity to initiate a fling, as measured in dips per second
      */
-    private static final int MAXIMUM_FLING_VELOCITY = 8000;
+    private static final int MAXIMUM_FLING_VELOCITY = 16000;
 
     /**
      * Delay before dispatching a recurring accessibility event in milliseconds.
