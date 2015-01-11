3c39f39d1e80d42f492fba586fae041695ffb71c
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-11 20:07:15 +0000

    QuickSettings: Option to disable brightness slider (1/2)
    Change-Id: I98a28e99e78ee4d8bb4f03bc53e867723d62b052
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -21,11 +21,14 @@ import static com.android.systemui.util.InjectionInflationController.VIEW_CONTEX
 
 import android.annotation.Nullable;
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.metrics.LogMaker;
 import android.os.Bundle;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
 import android.service.quicksettings.Tile;
@@ -54,6 +57,7 @@ import com.android.systemui.statusbar.policy.BrightnessMirrorController.Brightne
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.tuner.TunerService.Tunable;
 
+import android.provider.Settings;
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -66,7 +70,6 @@ import javax.inject.Named;
 public class QSPanel extends LinearLayout implements Tunable, Callback, BrightnessMirrorListener,
         Dumpable {
 
-    public static final String QS_SHOW_BRIGHTNESS = "qs_show_brightness";
     public static final String QS_SHOW_HEADER = "qs_show_header";
 
     private static final String TAG = "QSPanel";
@@ -80,6 +83,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
     protected boolean mExpanded;
     protected boolean mListening;
+
+    private SettingObserver mSettingObserver;
 
     private QSDetail.Callback mCallback;
     private BrightnessController mBrightnessController;
@@ -111,6 +116,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             DumpController dumpController) {
         super(context, attrs);
         mContext = context;
+
+        mSettingObserver = new SettingObserver(new Handler(context.getMainLooper()));
 
         setOrientation(VERTICAL);
 
@@ -178,8 +185,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        final TunerService tunerService = Dependency.get(TunerService.class);
-        tunerService.addTunable(this, QS_SHOW_BRIGHTNESS);
+        mSettingObserver.observe();
+        mSettingObserver.update();
 
         if (mHost != null) {
             setTiles(mHost.getTiles());
@@ -213,9 +220,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
     @Override
     public void onTuningChanged(String key, String newValue) {
-        if (QS_SHOW_BRIGHTNESS.equals(key)) {
-            updateViewVisibilityForTuningValue(mBrightnessView, newValue);
-        }
+        // No tunings for you.
     }
 
     private void updateViewVisibilityForTuningValue(View view, @Nullable String newValue) {
@@ -447,6 +452,33 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
     protected boolean shouldShowDetail() {
         return mExpanded;
+    }
+
+    private final class SettingObserver extends ContentObserver {
+        public SettingObserver(Handler handler) {
+            super(handler);
+        }
+
+         void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER),
+                    false, this, UserHandle.USER_ALL);
+            update();
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            update();
+        }
+
+        public void update() {
+            boolean brightnessSliderEnabled = Settings.Secure.getIntForUser(
+            mContext.getContentResolver(), Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER,
+                1, UserHandle.USER_CURRENT) == 1;
+            mBrightnessView.setVisibility(brightnessSliderEnabled ? View.VISIBLE : View.GONE);
+        }
     }
 
     protected TileRecord addTile(final QSTile tile, boolean collapsedView) {
