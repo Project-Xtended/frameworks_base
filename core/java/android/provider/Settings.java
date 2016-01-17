9ad922cca33b04820c593a4d3dc0481f7f7240fa
beanstown106 <nbenis106@gmail.com>
2016-01-17 09:14:19 -0500

    Long-press power while display is off for torch [1/2]
    Squash of:
    
    Author: beanstown106 <nbenis106@gmail.com>
    Date:   Sun Jan 17 09:14:19 2016 -0500
        policy: Long-press power while display is off for torch
        Long-press the power button while the display is off
        to turn the torchlight on and off.
        Credits:
         - Lion0738: The main hooks here:
           https://github.com/lion0738/android_frameworks_base/commit/9af2b7844a4d973c8b6c542d7937f56a24a7e5f1
         - Atlantis: The logic on where to hook into this for power button only
         - Alex Cruz: Helping and giving me some pointers along the way
        Change-Id: I14365389990eb06daaa127f5db66df45abf6c064
        (cherry picked from commit 604f00ce176463bc3e60b99195fc7a757f49f957)
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sat Dec 24 13:22:53 2016 -0800
        [1/3] Torch long press power: add auto-off function
        Change-Id: Icdf50082324f8292859f0df8b271e730b02c84e7
    
    Change-Id: I49222892c8fbc8a63af580c763e8987b225b11d2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4793,7 +4793,7 @@ public final class Settings {
         };  
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
 
-	    /**
+	/**
          * show the membar in recents
          *  @hide
          */
@@ -4904,6 +4904,16 @@ public final class Settings {
          * @hide
          */
         public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
+
+        /**
+         * @hide
+         */
+        public static final String TORCH_LONG_PRESS_POWER_GESTURE = "torch_long_press_power_gesture";
+
+        /**
+         * @hide
+         */
+        public static final String TORCH_LONG_PRESS_POWER_TIMEOUT = "torch_long_press_power_timeout";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
