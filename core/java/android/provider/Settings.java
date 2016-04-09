e165eba41a49c9e5b3bea432483712c88f147f66
Lokesh Chamane <lokesh.c703@gmail.com>
2016-04-09 19:54:40 -0400

    [1/2] base: HeadsUp snooze function
    5.1 introduced a snooze feature which activates when swiping up.
    Default snooze time is 1 minute. Let's make it configurable!
    
    Forward ported to marshmallow/Nougat By:@BeansTown106
    *pretty much a rewrite using the logic from my heads up timeout port
    *also added a second toast for 1 minute instead of saying 1 minutes
    included Make Heads Up snooze toast more informative by beanstown106 too
    *tell the user which app is snoozing
    *also center the toast to account for big app names
    *RU
    Example: SystemUI - Peeking notifications are snoozing for 1 minute.
    
    Forward ported to 10 by: @eyosen
    
    Change-Id: Ia6d0ab44eb49ba4f70a345150f7c40d64105e691

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5811,6 +5811,16 @@ public final class Settings {
         private static final Validator TRANSPARENT_POWER_DIALOG_DIM_VALIDATOR =
                new SettingsValidators.InclusiveIntegerRangeValidator(0, 100);
 
+       /**
+        * Defines the global heads up notification snooze
+        * @hide
+        */
+        public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
+
+        /** @hide */
+        private static final Validator HEADS_UP_NOTIFICATION_SNOOZE_VALIDATOR =
+              new SettingsValidators.InclusiveIntegerRangeValidator(0, 1200000);
+
         /**
          * Whether the user has already accepted MediaProjection permission for the built-in screenrecorder
          * @hide
@@ -5938,6 +5948,7 @@ public final class Settings {
             VOLUME_DIALOG_TIMEOUT,
             TRANSPARENT_POWER_MENU,
             TRANSPARENT_POWER_DIALOG_DIM,
+            HEADS_UP_NOTIFICATION_SNOOZE,
         };
 
         /**
@@ -6128,6 +6139,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VOLUME_DIALOG_TIMEOUT);
             PRIVATE_SETTINGS.add(TRANSPARENT_POWER_MENU);
             PRIVATE_SETTINGS.add(TRANSPARENT_POWER_DIALOG_DIM);
+            PRIVATE_SETTINGS.add(HEADS_UP_NOTIFICATION_SNOOZE);
         }
 
         /**
@@ -6286,6 +6298,7 @@ public final class Settings {
             VALIDATORS.put(VOLUME_DIALOG_TIMEOUT,VOLUME_DIALOG_TIMEOUT_VALIDATOR);
             VALIDATORS.put(TRANSPARENT_POWER_MENU,TRANSPARENT_POWER_MENU_VALIDATOR);
             VALIDATORS.put(TRANSPARENT_POWER_DIALOG_DIM,TRANSPARENT_POWER_DIALOG_DIM_VALIDATOR);
+            VALIDATORS.put(HEADS_UP_NOTIFICATION_SNOOZE,HEADS_UP_NOTIFICATION_SNOOZE_VALIDATOR);
         }
 
         /**
