a1eea1144348e3549fea8234c6e75c8e67fcb09f
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2016-05-14 20:31:42 -0400

    BatteryBar: Gradient color [1/2]
    Change-Id: I1c18e5e57d77faac8d347a751f8a9d2e63f97517
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4454,7 +4454,22 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String BATTERY_BAR_CHARGING_COLOR = "battery_bar_charging_color";
+        public static final String BATTERY_BAR_BATTERY_LOW_COLOR_WARNING = "battery_bar_battery_low_color_warning";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_HIGH_COLOR = "battery_bar_high_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_LOW_COLOR = "battery_bar_low_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_USE_GRADIENT_COLOR = "battery_bar_use_gradient_color";
 
         /**
          * @hide
