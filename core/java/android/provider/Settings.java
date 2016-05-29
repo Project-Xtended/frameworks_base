174cd2fd58a61f9ddbd1cb2f052b33cdfca61fcd
Daniel Huber <daniel.huber.business@gmail.com>
2016-05-29 21:49:37 +0200

    base: Screenshot Crop & Share
    Change-Id: Iedec3d89362612a60a57b07884cae9de3466d1a9
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5418,6 +5418,18 @@ public final class Settings {
        public static final String STATUS_BAR_FILE_HEADER_IMAGE = "status_bar_file_header_image";
 
         /**
+         * Define if screenshot crop & share is active
+         * @hide
+         */
+        public static final String SCREENSHOT_CROP_AND_SHARE = "screenshot_crop_and_share";
+
+        /**
+         * Define the crop behavior of screenshot crop & share
+         * @hide
+         */
+        public static final String SCREENSHOT_CROP_BEHAVIOR = "screenshot_crop_behavior";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
