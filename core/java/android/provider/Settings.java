e1e7c25269fc1ecc0e33993c087c8380f51ce560
Anas Karbila <anaskarbila@gmail.com>
2017-06-03 03:21:32 +0200

    PowerManagerService: Allow to light up buttons only when pressed
    Author: Anas Karbila <anaskarbila@gmail.com>
    Date:   Sat Jun 3 03:21:32 2017 +0200
    
        PowerManagerService: Allow to light up buttons only when pressed
    
         * Right now capactive, lit hardware keys are being
           lit every time you either touch them or the screen.
    
           But some devices handle this differently on stock:
    
           Display touch => buttons not lit
           Buttons touch => buttons lit
    
         * Thus, add a setting in order to allow the user
           to choose the preferred behavior.
    
        Change-Id: I35ac71a8274568901f962c9692788d1c682a98dd
    
    Author: Corinna Vinschen <xda@vinschen.de>
    Date:   Sun Aug 6 15:05:54 2017 +0200
    
        PowerManagerService: fix HW button illumination timeout
    
        Change I35ac71a8274568901f962c9692788d1c682a98dd, introducing hardware
        button backlight on button keypress only, also introduced a bug:
    
        When touching a button and then performing display activity while
        the buttons are still on, the buttons would keep lightened up until
        the next user interaction, potentially only switched off at the next
        screen off timeout.  Also, the buttons were not illuminated on
        device wakeup.
    
        This patch fixes it, together with another, long-standing problem:
    
        When touching a hardware button, nextTimeout was set to
        now + mButtonTimeout, even if mButtonTimeout is longer than the timeout
        determined by the screen off timeout.  To wit, if screen timeout is set
        to 15 secs, but button timeout to values > 15 secs.
    
        Change-Id: I8a56f1d1e0138c38ed6fe294e4816a9f7f744f1e
        Signed-off-by: Corinna Vinschen <xda@vinschen.de>
    
    Change-Id: I5b486e65a5b7d9d16590941df0af4d9c604dedc4
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4559,6 +4559,7 @@ public final class Settings {
         /**
          * Whether to display power in the power menu
          *
+
          * @hide
          */
         public static final String POWERMENU_POWER = "powermenu_power";
@@ -4627,6 +4628,17 @@ public final class Settings {
 
         /** @hide */
         public static final Validator BUTTON_BRIGHTNESS_VALIDATOR = NON_NEGATIVE_INTEGER_VALIDATOR;
+
+        /**
+         * Whether the button backlight is only lit when pressed (and not when screen is touched)
+         * The value is boolean (1 or 0).
+         */
+        public static final String BUTTON_BACKLIGHT_ONLY_WHEN_PRESSED =
+                "button_backlight_only_when_pressed";
+
+        /** @hide */
+        public static final Validator BUTTON_BACKLIGHT_ONLY_WHEN_PRESSED_VALIDATOR =
+                BOOLEAN_VALIDATOR;
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
@@ -4815,6 +4827,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(BUTTON_BACKLIGHT_TIMEOUT);
+            PRIVATE_SETTINGS.add(BUTTON_BACKLIGHT_ONLY_WHEN_PRESSED);
             PRIVATE_SETTINGS.add(BUTTON_BRIGHTNESS);
         }
 
@@ -4913,6 +4926,7 @@ public final class Settings {
             VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
             VALIDATORS.put(BUTTON_BACKLIGHT_TIMEOUT, BUTTON_BACKLIGHT_TIMEOUT_VALIDATOR);
             VALIDATORS.put(BUTTON_BRIGHTNESS, BUTTON_BRIGHTNESS_VALIDATOR);
+            VALIDATORS.put(BUTTON_BACKLIGHT_ONLY_WHEN_PRESSED, BUTTON_BACKLIGHT_ONLY_WHEN_PRESSED_VALIDATOR);
         }
 
         /**
