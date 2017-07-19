8635ba73620909f49298cba7aedfb3baaa3597d9
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    Allow using 4G icon instead of LTE [1/2]
    Change-Id: Ieb912c64a60ddd42fe7b21d18dadea293ff159c5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5523,6 +5523,12 @@ public final class Settings {
         public static final String TEXT_CHARGING_SYMBOL = "text_charging_symbol";
 
         /**
+         * Whether to display 4G icon instead LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
