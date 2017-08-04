5e2a99e6e2b0650a5e924de5c57eb4f7a964ac60
LorDClockaN <lordclockan@gmail.com>
2017-08-04 15:43:57 +0200

    FWB: Sensor block per-package switch (1/2)
    Change-Id: I3a5cb8820ca6815c7140c25368e525faa2ecc5e6
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7023,6 +7023,12 @@ public final class Settings {
         public static final String KEYGUARD_QUICK_TOGGLES_NEW = "keyguard_quick_toggles_new";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
