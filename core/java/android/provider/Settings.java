8269330101c9450df3bbd53c42761063899cc5cd
ezio84 <brabus84@gmail.com>
2017-09-06 23:59:02 +0200

    Allow to disable qs footer warnings [1/2]
    When using apps like AdGuard that create a vpn, a warning is displayed
    in the QS panel footer, so the user knows the device net is being monitored
    by an external app.
    The warning takes space and the user might want to avoid it if he already
    knows that the app is running (also because there's a statusbar icon
    for the same purpose)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3981,6 +3981,20 @@ public final class Settings {
         private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR = sBooleanValidator;
 
         /**
+        * Whether to show Qs panel footer warnings like for active VPN
+        * @hide
+        */
+       public static final String QS_FOOTER_WARNINGS = "qs_footer_warnings";
+
+        /**
+         * IMPORTANT: If you add a new public settings you also have to add it to
+         * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
+         * it to PRIVATE_SETTINGS below. Also add a validator that can validate
+         * the setting value. See an example above.
+         */
+
+        /**
+         * Whether to use the MTP by default after connecting to PC
          * @hide
          */
         public static final String SHOW_BATTERY_IMAGE = "status_bar_show_battery_image";
