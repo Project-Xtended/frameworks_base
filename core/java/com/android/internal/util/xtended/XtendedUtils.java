fd3055453c6043e65c579924809cd00b4607dec8
bigrushdog <randall.rushing@gmail.com>
2017-09-06 20:46:51 +0200

    core: Add api to take screenshots
    Omni: extracted from DUI
    add a public call to XtendedUtils
    
    Change-Id: If71944466c703a9b3fe54aea6bb21150b0a082bb
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/com/android/internal/util/xtended/XtendedUtils.java
+++ core/java/com/android/internal/util/xtended/XtendedUtils.java
@@ -32,6 +32,8 @@ import android.view.WindowManagerGlobal;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
+import android.net.ConnectivityManager;
+
 
 import com.android.internal.statusbar.IStatusBarService;
 
@@ -39,6 +41,9 @@ import com.android.internal.statusbar.IStatusBarService;
  * Some custom utilities
  */
 public class XtendedUtils {
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
@@ -129,6 +134,14 @@ public class XtendedUtils {
         context.sendBroadcastAsUser(keyguardIntent, user);
     }
 
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
