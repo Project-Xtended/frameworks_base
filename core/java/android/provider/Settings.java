e3a0466d90d5d2a36723838588a5a69353a72c40
thecrazyskull <anaskarbila@aospa.co>
2017-09-11 10:09:44 +0800

    PowerManagerService: add a config to light up buttons only when press… …ed (1/2)
     * Right now capactive, lit hardware keys are being
       lit every time you either touch them or the screen.
    
       But some devices handle this differently on stock:
    
       Display touch => buttons not lit
       Buttons touch => buttons lit
    
    reworked with switch (extracted from omnirom/android_frameworks_base@5d1fd91) by @xyyx

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3319,6 +3319,11 @@ public final class Settings {
         public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
        
         /**
+         * @hide
+         */
+        public static final String BUTTON_BACKLIGHT_ON_TOUCH_ONLY = "button_backlight_on_touch_only";
+
+        /**
          * Control whether the process CPU usage meter should be shown.
          *
          * @deprecated This functionality is no longer available as of
