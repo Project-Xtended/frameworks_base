32a4f420d6f20cdf8957c9ada66efe567f8e2525
Roman Birg <roman@cyngn.com>
2017-09-18 14:40:57 +0000

    base: SystemUI: add volume tones and toggle back [1/2] thanks to @romanbb & default round system icon thanks to @ezio84
    During the move from VolumePanel to the new VolumeController/Dialog
    setup, the volume tone logic got removed.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4233,6 +4233,12 @@ public final class Settings {
          */
         public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
 
+         /**
+         * Whether the phone volume up / down effect should be played
+         * @hide
+         */
+        public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
+
         /**
          * Whether the phone vibrates on disconnect
          * @hide
