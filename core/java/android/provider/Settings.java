0f2c496c7e1f6b4cd7a2f09dad24eb42ae117639
LorDClockaN <lordclockan@gmail.com>
2017-09-19 19:27:58 +0800

    Powermenu Torch option [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4328,6 +4328,13 @@ public final class Settings {
         public static final String POWERMENU_USERS = "powermenu_users";
 
         /**
+         * Whether to display the torch option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_TORCH = "powermenu_torch";
+
+        /**
          * Boolean value whether to link ringtone and notification volume
          *
          * @hide
