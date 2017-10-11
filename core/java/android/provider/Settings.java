ea776612720390adf33c4ab06c4480702a02a412
LorDClockaN <lordclockan@gmail.com>
2017-10-11 14:30:25 +0200

    Toast icon switch [1/2]
    @neobuddy89: Moved to global settings.
    
    Change-Id: I59ffd10f20a2ae9491f76380ec9af641e0844928
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11417,6 +11417,12 @@ public final class Settings {
         public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
 
         /**
+         * Toast icon switch
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /*
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
