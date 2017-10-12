30c8b172dc1e7474ca2a9f49644d72913aa6167c
maxwen <max.weninger@gmail.com>
2017-10-12 20:24:01 +0200

    [SQUASHED] Status bar logo [1/2]
    This is Merge commit of
    
    Validus:-
    1) https://github.com/ValidusOs/frameworks_base/commit/738711fb1ad59d6136538fccdc7b257f9a33ec68#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
    2) https://github.com/ValidusOs/frameworks_base/commit/16f05ef9f810faef2e515bfd6ad69fa7a2e062b0#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
    3) https://github.com/ValidusOs/frameworks_base/commit/2e78a613ee3d4ac2c01276d5f7ffed0fc2f3fe86#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
    4) https://github.com/ValidusOs/frameworks_base/commit/576d1610b2f1056034ea9a5058fb73225d31dc71#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
    5) https://github.com/ValidusOs/frameworks_base/commit/4cae0470cfbe21f126f4e54f13051edb5feec1d9#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
    6) https://github.com/ValidusOs/frameworks_base/commit/4b3ad92c4a6f86e51d45c4b57e4faa84b3f560d1#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
    7) https://github.com/ValidusOs/frameworks_base/commit/3b2ea41b645d2ffb72eef69e621435af8a84624b#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
    8) https://github.com/ResurrectionRemix/android_frameworks_base/commit/f939656166ad1ebb4ff7ce5af00393cb9efc93cb#diff-45fca3a0730ef7bdbfcba8f4fd0e9c0b
    
    CrDroid Android:-
    Thanks @neobuddy89 For Logos
    
    1) https://github.com/crdroidandroid/android_frameworks_base/commit/a09738a65fdb93bb8c428124dc6a2a621fab2db8
    
    Change-Id: I86eb407557197229481940e565ab711eb56312b0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5902,6 +5902,25 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
+         * 0 = off
+	 * 1 = left
+	 * 2 = right
+         */
+        public static final String STATUS_BAR_LOGO = "status_bar_logo";
+
+        /**
+	 ** Statusbar logo Style
+	 ** @hide
+	 **/
+	 public static final String STATUS_BAR_LOGO_STYLE = "status_bar_logo_style";
+
+	/**
+	 ** Statusbar logo Color
+	 ** @hide
+         **/
+	public static final String STATUS_BAR_LOGO_COLOR = "status_bar_logo_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6223,6 +6242,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(LESS_BORING_HEADS_UP);
             PRIVATE_SETTINGS.add(WIRELESS_CHARGING_ANIMATION);
             PRIVATE_SETTINGS.add(NOTIFICATION_GUTS_KILL_APP_BUTTON);
+            PRIVATE_SETTINGS.add(STATUS_BAR_LOGO);
         }
 
         /**
