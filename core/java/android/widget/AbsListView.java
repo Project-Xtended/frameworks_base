4ddceb9b366b7ccf727495f017e8a77396fd2906
Evan Rosky <erosky@google.com>
2017-10-26 12:50:33 -0700

    Fix some list-item selection/scrolling issues
    Any "touch" interaction now hides selection (since mouse
    doesn't enable touch-mode, this wasn't happening anymore;
    also, allowing listview to scroll an actual selection
    off-screen would be very involved and risky).
    
    The selector hilight remains (since mouse doesn't enter
    touch-mode). This is a new scenario, so this change also
    makes sure to hide the selector hilight when it's target
    view is scrolled off-screen.
    
    Bug: 67881712
    Bug: 67720587
    Test: Existing CTS tests still pass. Can now click list items
          via mouse even when scrolled. Visually checked that that
          selector hilight is not visible if off-screen.
    Change-Id: Ia7b0fd7b247e8d9d9e609364a5500717df648fd9
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -4105,6 +4105,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         mHasPerformedLongPress = false;
         mActivePointerId = ev.getPointerId(0);
         mInverse.sendEmptyMessageDelayed(0, 100);
+        hideSelector();
         if (mTouchMode == TOUCH_MODE_OVERFLING) {
             // Stopped the fling. It is a scroll.
             mFlingRunnable.endFling();
