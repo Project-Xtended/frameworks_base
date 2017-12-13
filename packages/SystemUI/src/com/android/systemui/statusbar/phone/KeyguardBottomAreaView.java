94816dde8c4e66ab9f3d5ce87312dc687577a2b9
ezio84 <brabus84@gmail.com>
2017-12-13 11:17:55 +0100

    base: Fix custom lockscreen shortcut icons being white
    now 8.1 sets a tint to make icons black and white according to the
    wallpaper main color but this is not suitable for custom icons
    let's fix this setting the tint only for default lockscreen buttons

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -280,6 +280,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         updateCameraIconColor();
         updatePhoneIconColor();
         updateIndicationTextColor();
+        updateRightAffordance();
     }
 
     @Override
@@ -369,8 +370,10 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         mRightAffordanceView.setVisibility(!mDozing && state.isVisible ? View.VISIBLE : View.GONE);
         if (state.isVisible) {
             if (state.drawable != mRightAffordanceView.getDrawable()
-                    || state.tint != mRightAffordanceView.shouldTint()) {
-                mRightAffordanceView.setImageDrawable(state.drawable, state.tint);
+                    || state.tint != mRightAffordanceView.shouldTint()
+		    || !state.isDefaultButton) {
+                mRightAffordanceView.setImageDrawable(state.drawable, state.tint,
+                    state.isDefaultButton ? false : true);
             }
             mRightAffordanceView.setContentDescription(state.contentDescription);
         }
@@ -392,6 +395,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         mUserSetupComplete = userSetupComplete;
         updateCameraVisibility();
         updateLeftAffordanceIcon();
+        updateRightAffordanceIcon();
     }
 
     private Intent getCameraIntent() {
@@ -443,8 +447,10 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         mLeftAffordanceView.setVisibility(state.isVisible ? View.VISIBLE : View.GONE);
         if (state.isVisible) {
             if (state.drawable != mLeftAffordanceView.getDrawable()
-                    || state.tint != mLeftAffordanceView.shouldTint()) {
-                mLeftAffordanceView.setImageDrawable(state.drawable, state.tint);
+                    || state.tint != mLeftAffordanceView.shouldTint()
+                    || !state.isDefaultButton) {
+                mLeftAffordanceView.setImageDrawable(state.drawable, state.tint,
+                    state.isDefaultButton ? false : true);
             }
             mLeftAffordanceView.setContentDescription(state.contentDescription);
 	}
@@ -813,9 +819,9 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
     private void setRightButton(IntentButton button) {
         mRightButton = button;
-        updateRightAffordanceIcon();
         updateCameraVisibility();
         inflateCameraPreview();
+        updateRightAffordance();
     }
 
     private void setLeftButton(IntentButton button) {
@@ -832,6 +838,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
         updateCameraVisibility();
         updateLeftAffordanceIcon();
+        updateRightAffordanceIcon();
 
         if (dozing) {
             mOverlayContainer.setVisibility(INVISIBLE);
