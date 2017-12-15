f0e67b32e7237afe50c9b28b4eb18bb7c77dada5
xyyx <xyyx@mail.ru>
2017-12-15 20:32:18 +0800

    Fix screenshot action in powermenu * and move delay to NitrogenUtils * decrease screenrecord tile delay

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -636,22 +636,23 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
 
         @Override
         public void onPress() {
-            /* wait for the dialog box to close */
-            try {
-                 Thread.sleep(1000); //1s
-            } catch (InterruptedException ie) {}
-            NitrogenUtils.takeScreenshot(true);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    NitrogenUtils.takeScreenshot(true);
+                }
+            }, 500);
         }
-
 
         @Override
         public boolean onLongPress() {
             mHandler.sendEmptyMessage(MESSAGE_DISMISS);
-            /* wait for the dialog box to close */
-            try {
-                 Thread.sleep(1000); //1s
-            } catch (InterruptedException ie) {}
-            NitrogenUtils.takeScreenshot(false);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    NitrogenUtils.takeScreenshot(false);
+                }
+            }, 500);
             return true;
         }
 
@@ -671,13 +672,12 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
                 com.android.systemui.R.string.global_action_screenrecord) {
 
             public void onPress() {
-                mHandler.sendEmptyMessage(MESSAGE_DISMISS);
-                /* wait for the dialog box to close */
-                try {
-                     Thread.sleep(1000); //1s
-                } catch (InterruptedException ie) {}
-
-                takeScreenrecord();
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        takeScreenrecord();
+                    }
+                }, 500);
             }
 
             public boolean showDuringKeyguard() {
