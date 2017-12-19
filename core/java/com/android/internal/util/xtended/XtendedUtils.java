aeaa0e7eac4e807160625e442cd2df96e215a032
ezio84 <brabus84@gmail.com>
2017-12-19 14:37:35 +0100

    screenshot: Append app name to screenshot file
    kanging Samsung idea :O
    
    Change-Id: Ibc6a52b5e2597e6014a2da6a4211febe17ec02c7
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/com/android/internal/util/xtended/XtendedUtils.java
+++ core/java/com/android/internal/util/xtended/XtendedUtils.java
@@ -16,9 +16,13 @@
 
 package com.android.internal.util.xtended;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.hardware.input.InputManager;
 import android.os.Handler;
 import android.os.Looper;
@@ -34,6 +38,7 @@ import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.net.ConnectivityManager;
 
+import java.util.List;
 
 import com.android.internal.statusbar.IStatusBarService;
 
@@ -141,6 +146,22 @@ public class XtendedUtils {
         } catch (RemoteException e) {
             e.printStackTrace();
         }
+    }
+
+    public static ActivityInfo getRunningActivityInfo(Context context) {
+        final ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        final PackageManager pm = context.getPackageManager();
+
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            ActivityManager.RunningTaskInfo top = tasks.get(0);
+            try {
+                return pm.getActivityInfo(top.topActivity, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+        return null;
     }
 }
 
