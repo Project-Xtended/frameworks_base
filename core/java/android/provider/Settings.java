04b8b44e53334bfe5b186dd85fc6b4cfbc10ea9f
maxwen <max.weninger@gmail.com>
2017-12-20 14:38:41 +0000

    [1/2] base: configure SystemUI theme
    allow to force light or dark or use the wallpaper based
    automatic selection
    
    [Kshitij Gupta]
    - Adapt for extra DarkUI overlays
    - Refactor some variables. We're theming System, not just
      SystemUI
    
    edit: adapt for P and disable the aosp systemui picker
    
    Change-Id: Ie74ee40c7c054b259a6fb907ab83060769b6d8dd
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5179,6 +5179,14 @@ public final class Settings {
         public static final String INCALL_NOTIFICATIONS_VIBRATE = "incall_notifications_vibrate";
 
         /**
+         * 0 - wallpaper based
+         * 1 - force light
+         * 2 - force dark
+         * @hide
+         */
+        public static final String SYSTEM_THEME_STYLE = "system_theme_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5398,6 +5406,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(BURN_IN_PROTECTION);
             PRIVATE_SETTINGS.add(BURN_IN_PROTECTION_INTERVAL);
             PRIVATE_SETTINGS.add(MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD);
+            PRIVATE_SETTINGS.add(SYSTEM_THEME_STYLE);
         }
 
 
