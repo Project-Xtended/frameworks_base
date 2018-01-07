f04eed801dd52a4e520b90588a1e3f0e1861f82b
Pranav Vashi <neobuddy89@gmail.com>
2018-01-07 16:28:16 +0530

    Recents: Add deep clear feature for dismiss button
    * Credits to @ezio84 for his work here:
    https://github.com/ezio84/abc_frameworks_base/commit/0ef34b0164ac4f58cbcaea9c20fd8a4e1eec6bd0
    * This patch rewires force close feature to existing dismiss button when enabled.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5059,6 +5059,12 @@ public final class Settings {
         public static final String RECENTS_ICON_PACK = "recents_icon_pack";
 
         /**
+         * Whether to kill app when using dismiss button in recents
+         * @hide
+         */
+        public static final String RECENTS_DEEP_CLEAR = "recents_deep_clear";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
