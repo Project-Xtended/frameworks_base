01f021b70c380354716d68056f77db4aa020c09d
SpiritCroc <spiritcroc@gmail.com>
2018-01-13 12:16:46 +0100

    Add Always On Display QS Tile
    @neobuddy89: Simplify and fix multiuser setting.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -24,6 +24,7 @@ import com.android.systemui.plugins.qs.*;
 import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
+import com.android.systemui.qs.tiles.AlwaysOnDisplayTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CaffeineTile;
@@ -146,6 +147,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SoundTile(mHost);
             case "music":
                 return new MusicTile(mHost);
+            case "always_on_display":
+                return new AlwaysOnDisplayTile(mHost);
         }
 
         // Intent tiles.
