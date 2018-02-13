5a0ca0ca53be9d1318cc521fbdb28d3d52c0d293
ezio84 <brabus84@gmail.com>
2018-02-13 11:54:36 +0100

    Circle and dot battery fixes and improvements
    fix/add bolt and warning indicators
    add percentage inside
    make txt percentage dynamic when charging or not
    hide inside percentage when forcing txt percentage
    other fixes and improvements
    
    Change-Id: I3ee52eb69d4588e3b01ca8e1cefc42f34a35c6c8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8186,8 +8186,10 @@ public final class Settings {
          * 1: Display the battery an icon in landscape mode
          * 2: Display the battery as a circle
          * 3: Display the battery as a dotted circle
-         * 4: Display the battery as text
-         * 5: Do not display the battery
+         * 4: Display the battery as a big circle and show level into the icon
+         * 5: Display the battery as a big dotted circle and show level into the icon
+         * 6: Display the battery as text
+         * 7: Do not display the battery
          * default: 0
          * @hide
          */
