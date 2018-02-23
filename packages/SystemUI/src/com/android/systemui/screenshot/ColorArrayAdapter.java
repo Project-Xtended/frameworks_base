d7d026acf05b3d1afa7f7212dcb36c8c05139224
SuperDroidBond <superdroidbond@yahoo.com>
2018-02-23 17:27:22 +0000

    Revert "Screenshot Crop & Share: add draw stroke width support"
    This reverts commit 44d1ea458fa0e7b98b12fa93233b54ea05cc8846.

--- packages/SystemUI/src/com/android/systemui/screenshot/ColorArrayAdapter.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ColorArrayAdapter.java
@@ -1,12 +1,9 @@
 package com.android.systemui.screenshot;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PaintFlagsDrawFilter;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -42,23 +39,12 @@ public class ColorArrayAdapter extends ArrayAdapter<Integer> {
 
     private ImageView createColorImage(int position){
         ImageView imageView = new ImageView(getContext());
-        final int width = getContext().getResources().getDimensionPixelSize(R.dimen.crop_buttons);
-        final int contentWidth = getContext().getResources().getDimensionPixelSize(R.dimen.crop_buttons_inlet);
-        final int color = colors[position];
-
-        final Canvas canvas = new Canvas();
-        canvas.setDrawFilter(new PaintFlagsDrawFilter(Paint.ANTI_ALIAS_FLAG,Paint.FILTER_BITMAP_FLAG));
-        final Bitmap bmp = Bitmap.createBitmap(contentWidth, contentWidth, Bitmap.Config.ARGB_8888);
-        canvas.setBitmap(bmp);
-        final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(color);
-        canvas.drawRect(0, 0, contentWidth, contentWidth, paint);
-
-        imageView.setImageDrawable(new BitmapDrawable(getContext().getResources(), bmp));
-        AbsListView.LayoutParams params = new AbsListView.LayoutParams(width, width);
+        int colorWidth = getContext().getResources().getDimensionPixelSize(R.dimen.crop_color_rect);
+        ColorDrawable colorRect = new ColorDrawable(colors[position]);
+        imageView.setImageDrawable(colorRect);
+        AbsListView.LayoutParams params = new AbsListView.LayoutParams(colorWidth, colorWidth);
         imageView.setLayoutParams(params);
-        imageView.setScaleType(ScaleType.CENTER);
+        imageView.setScaleType(ScaleType.CENTER_INSIDE);
         return imageView;
     }
 }
