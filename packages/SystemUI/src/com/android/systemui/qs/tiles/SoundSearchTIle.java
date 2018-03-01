82a2e9180091a29fa6daa94ad1d5b669bbb7a136
SuperDroidBond <superdroidbond@yahoo.com>
2018-03-01 10:30:26 +0400

    Merge branch 'xtended' into xtended

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SoundSearchTIle.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SoundSearchTIle.java
@@ -39,7 +39,7 @@ public class SoundSearchTIle extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.NITROGEN_SETTINGS;
+        return MetricsEvent.XTENDED;
     }
 
     @Override
