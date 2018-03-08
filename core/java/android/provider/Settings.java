9940bd918c4b0f80e325cf972da4feab2eb267c4
SuperDroidBond <superdroidbond@yahoo.com>
2018-03-08 11:57:19 +0000

    Sensor Block Per Packages [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4712,6 +4712,12 @@ public final class Settings {
         public static final String SCREENSHOT_TYPE = "screenshot_type";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        public static final String SENSOR_BLOCK = "sensor_block";
+        
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
