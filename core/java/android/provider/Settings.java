99fe78a8a2419bbe3948ce80ea6d61099527605d
Pranav Vashi <neobuddy89@gmail.com>
2018-04-02 00:10:08 +0530

    Add toggle to enable Android P system animation [1/2]
    Change-Id: Iabef2773c54aee04bea34ed42b04b14cb6c65aa1
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11792,6 +11792,12 @@ public final class Settings {
         public static final String DEVICE_IDLE_CONSTANTS_USER = "device_idle_constants_user";
 
         /**
+         * Default animation
+         * @hide
+         */
+        public static final String SYSTEM_DEFAULT_ANIMATION = "system_default_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
