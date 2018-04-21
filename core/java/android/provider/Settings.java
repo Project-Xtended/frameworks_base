bfd83b41d0d35fe1385b14fd268fb62613f44cf3
Ded_Boi <dhanush911gtr@gmail.com>
2018-04-21 07:58:22 +0000

    Revert "AOKP custom system animations (1/2)"
    This reverts commit a49e00ddb5678e64c44f994c0dadbd3ce5ab8979.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4312,24 +4312,6 @@ public final class Settings {
           */
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
-        /**  
-         * AOKP Custom System Animations
-         * @hide
-         */  
-        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
-                "activity_open",
-                "activity_close",
-                "task_open",  
-                "task_close",  
-                "task_to_front",
-                "task_to_back",  
-                "wallpaper_open",  
-                "wallpaper_close",  
-                "wallpaper_intra_open",  
-                "wallpaper_intra_close",  
-        };  
-        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
-
         /**
          * Whether to wake the screen with the volume keys, the value is boolean.
          * @hide
