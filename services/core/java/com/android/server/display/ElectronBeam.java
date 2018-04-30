791628aa489286c4db4b66ab8d0bc14f2a77d8cf
maxwen <max.weninger@gmail.com>
2018-04-30 15:12:38 +0200

    base: catch what you cant explain
    E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: android.ui
    E AndroidRuntime: java.lang.NullPointerException: mNativeObject is null. Have you called release() already?
    E AndroidRuntime: at android.view.SurfaceControl.checkNotReleased(SurfaceControl.java:417)
    E AndroidRuntime: at android.view.SurfaceControl.setPosition(SurfaceControl.java:485)
    E AndroidRuntime: at com.android.server.display.ElectronBeam.onDisplayTransaction(ElectronBeam.java:786)
    
    Change-Id: Id9484591798d2f7f4970d877cb8d29a70218a79a
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/display/ElectronBeam.java
+++ services/core/java/com/android/server/display/ElectronBeam.java
@@ -773,25 +773,28 @@ final class ElectronBeam implements ScreenStateAnimator {
                     return;
                 }
 
-                DisplayInfo displayInfo = mDisplayManagerInternal.getDisplayInfo(mDisplayId);
-                switch (displayInfo.rotation) {
-                    case Surface.ROTATION_0:
-                        t.setPosition(mSurfaceControl, 0, 0);
-                        t.setMatrix(mSurfaceControl, 1, 0, 0, 1);
-                        break;
-                    case Surface.ROTATION_90:
-                        t.setPosition(mSurfaceControl, 0, displayInfo.logicalHeight);
-                        t.setMatrix(mSurfaceControl, 0, -1, 1, 0);
-                        break;
-                    case Surface.ROTATION_180:
-                        t.setPosition(mSurfaceControl, displayInfo.logicalWidth,
-                                displayInfo.logicalHeight);
-                        t.setMatrix(mSurfaceControl, -1, 0, 0, -1);
-                        break;
-                    case Surface.ROTATION_270:
-                        t.setPosition(mSurfaceControl, displayInfo.logicalWidth, 0);
-                        t.setMatrix(mSurfaceControl, 0, 1, -1, 0);
-                        break;
+                try {
+                    DisplayInfo displayInfo = mDisplayManagerInternal.getDisplayInfo(mDisplayId);
+                    switch (displayInfo.rotation) {
+                        case Surface.ROTATION_0:
+                            mSurfaceControl.setPosition(0, 0);
+                            mSurfaceControl.setMatrix(1, 0, 0, 1);
+                            break;
+                        case Surface.ROTATION_90:
+                            mSurfaceControl.setPosition(0, displayInfo.logicalHeight);
+                            mSurfaceControl.setMatrix(0, -1, 1, 0);
+                            break;
+                        case Surface.ROTATION_180:
+                            mSurfaceControl.setPosition(displayInfo.logicalWidth,
+                                    displayInfo.logicalHeight);
+                            mSurfaceControl.setMatrix(-1, 0, 0, -1);
+                            break;
+                        case Surface.ROTATION_270:
+                            mSurfaceControl.setPosition(displayInfo.logicalWidth, 0);
+                            mSurfaceControl.setMatrix(0, 1, -1, 0);
+                            break;
+                    }
+                } catch (Exception e) {
                 }
             }
         }
