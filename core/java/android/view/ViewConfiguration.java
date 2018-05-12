bea4a95d9e030687f34e6f72f21a0110535e74c6
Dabug123 <clarkbrunswick55@gmail.com>
2018-05-12 16:52:57 +0200

    extreme scrolling power
    Aparently we are keeping this authorship intact by a commit message so xdev edited ,alex edited,paul edited again and again,ryan edited lol

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -199,7 +199,7 @@ public class ViewConfiguration {
     /**
      * Maximum velocity to initiate a fling, as measured in dips per second
      */
-    private static final int MAXIMUM_FLING_VELOCITY = 8000;
+    private static final int MAXIMUM_FLING_VELOCITY = 18000;
 
     /**
      * Delay before dispatching a recurring accessibility event in milliseconds.
@@ -219,7 +219,7 @@ public class ViewConfiguration {
     /**
      * The coefficient of friction applied to flings/scrolls.
      */
-    private static final float SCROLL_FRICTION = 0.005f;
+    private static final float SCROLL_FRICTION = 0.001f;
 
     /**
      * Max distance in dips to overscroll for edge effects
