49b7ec5a22f9635879f4ae7f32777de03a45d3e9
Varun Date <date.varun123@gmail.com>
2018-05-17 02:48:51 +0000

    base: Lockscreen Date Fonts [1/2]
    Good Job @eldainosor. I was confused because of sliceview shit.
    Bootleggers (@eldainosor) edit: Update the code to use the mKeyguardSlice instead of mDateView (deprecated due to google switching all the ls date section to slices) and make a function to apply the same typeface to every view on such SliceView
    
    SDB: Call SliceView(DateView) Fonts with separate method. It looks neat and clean
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5566,6 +5566,13 @@ public final class Settings {
         private static final Validator LOCKSCREEN_WEATHER_SHOW_CITY_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Change fonts for the system lockscreen date
+         *
+         * @hide
+         */
+        public static final String LOCK_DATE_FONTS = "lock_date_fonts";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
