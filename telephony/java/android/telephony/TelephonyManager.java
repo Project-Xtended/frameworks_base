e2a5aa88678f4a7fac09449842f454dbe666786f
danny <noreference@web.de>
2018-06-20 19:43:42 +0200

    base: Privacy Guard for O [AOSP] [1/3]
    @mcdachpappe:
    * port from LineageOS to AOSP
    * contains the following commits:
    
    * appops: Privacy Guard for O
    https://github.com/LineageOS/android_frameworks_base/commit/c0f33379d860b97baea832c157692619bd044f58
    by @Steve Kondik
    
    * CMSettings: add privacy guard default key
    https://github.com/LineageOS/android_lineage-sdk/commit/f61f9d079f5fb420446fd0c84a213eb87ec672f3
    by @romanbb
    
    * Allow disabling the privacy guard notification - port from cm-12.1 (3/3)
    https://github.com/LineageOS/android_lineage-sdk/commit/ba670bf88fb3ad2de948fc0a4e4a6e257a92f8ad
    by @chirayudesai
    
    Original authors:
       - Sam Mortimer
       - Danesh Mondegarian
       - Jorge Ruesga
       - Diogo Ferreira
       - Roman Birg
       - Adnan Begovic
       - Chirayu Desai
       - Lars Greiss
       - Steve Kondik
       - CodeAurora

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -1,4 +1,7 @@
 /*
+ * Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
+ *
  * Copyright (C) 2008 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,6 +31,7 @@ import android.annotation.SystemApi;
 import android.annotation.SystemService;
 import android.annotation.WorkerThread;
 import android.app.ActivityThread;
+import android.app.AppOpsManager;
 import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -5619,6 +5623,13 @@ public class TelephonyManager {
     public void setDataEnabled(int subId, boolean enable) {
         try {
             Log.d(TAG, "setDataEnabled: enabled=" + enable);
+            AppOpsManager appOps = (AppOpsManager)mContext.getSystemService(Context.APP_OPS_SERVICE);
+            if (enable) {
+                if (appOps.noteOp(AppOpsManager.OP_DATA_CONNECT_CHANGE) != AppOpsManager.MODE_ALLOWED) {
+                    Log.w(TAG, "Permission denied by user.");
+                    return;
+                }
+            }
             ITelephony telephony = getITelephony();
             if (telephony != null)
                 telephony.setDataEnabled(subId, enable);
