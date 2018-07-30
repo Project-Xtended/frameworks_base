501b54f26ffdd1dfd41cf14cda9d1519fc965e5e
SuperDroidBond <superdroidbond@yahoo.com>
2018-07-31 02:25:42 +0530

    QS Tile Icon Accent Toggle [1/2]
    Idea taken from -: https://github.com/DotOS/android_frameworks_base/commit/8976f6176971c79ecc46a23475364fcd407bbd4d

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -393,28 +393,29 @@ public abstract class QSTileImpl<TState extends State> implements QSTile {
 
     public static int getColorForState(Context context, int state) {
         
-        boolean enableQsTileTinting = context.getResources().getBoolean(R.bool.config_enable_qs_tile_tinting);
+        boolean enableQsTileTinting = Settings.System.getInt(context.getContentResolver(), 
+			           Settings.System.QS_TILE_TINTING_ENABLE, 1) == 1;
 
         switch (state) {
             case Tile.STATE_UNAVAILABLE:
-                if (!enableQsTileTinting) {    
+                if (enableQsTileTinting) {    
                     return Utils.getDisabled(context,
-                            Utils.getColorAttr(context, android.R.attr.colorForeground));
+		            Utils.getColorAttr(context, android.R.attr.colorForeground));
                 } else {
                     return Utils.getDisabled(context,
 		            Utils.getColorAttr(context, android.R.attr.colorForeground)); 
                 }
             case Tile.STATE_INACTIVE:
-                if (!enableQsTileTinting) {
+                if (enableQsTileTinting) {
                     return Utils.getColorAttr(context, android.R.attr.textColorHint);
                 } else {
                     return Utils.getColorAttr(context, android.R.attr.textColorHint);
                 }
             case Tile.STATE_ACTIVE:
-                if (!enableQsTileTinting) {
-                    return Utils.getColorAttr(context, android.R.attr.textColorPrimary); 
+                if (enableQsTileTinting) {
+		    return context.getResources().getColor(R.color.qs_tiles_active_tint);
                 } else {
-                    return context.getResources().getColor(R.color.qs_tiles_active_tint);
+		    return Utils.getColorAttr(context, android.R.attr.textColorPrimary);
                 }
             default:
                 Log.e("QSTile", "Invalid state " + state);
