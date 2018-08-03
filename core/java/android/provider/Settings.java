benschhold/android_frameworks_base

Remove dashboard tile summaries [1/3]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/android/provider/Settings.java
@@ -3740,6 +3740,12 @@ public final class Settings {
         public static final String DISABLE_SETTINGS_SUGGESTIONS = "disable_settings_suggestions";
 
         /**
+         * Remove tile summaries in settings
+         * @hide
+         */
+        public static final String REMOVE_SETTINGS_SUMMARY = "remove_settings_summary";
+
+        /**
          * Whether navigation bar is enabled or not
          * @hide
          */

Project-Xtended/frameworks_base

core/java/android/provider/Settings.java
@@ -5938,6 +5938,12 @@ public final class Settings {
         public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
 
         /**
+         * Remove tile summaries in settings
+         * @hide
+         */
+        public static final String REMOVE_SETTINGS_TILE_SUMMARY = "remove_settings_tile_summary";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *