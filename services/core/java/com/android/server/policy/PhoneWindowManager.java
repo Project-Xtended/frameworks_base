66f555f1786a1da9ab323299b512c4f5e0b1a848
Alex <alex@dirtyunicorns.com>
2018-08-06 20:38:00 -0500

    Merge tag 'android-8.1.0_r43' of https://android.googlesource.com/platform//frameworks/base into HEAD
    Android 8.1.0 release 43
    
    Change-Id: I92af9d74a15f0caf40dd69b83da84a2a46f8ee56

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2666,6 +2666,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     public void onTrustedChanged() {
                         mWindowManagerFuncs.notifyKeyguardTrustedChanged();
                     }
+
+                    @Override
+                    public void onShowingChanged() {
+                        mWindowManagerFuncs.onKeyguardShowingAndNotOccludedChanged();
+                    }
                 });
 
         final Resources res = mContext.getResources();
