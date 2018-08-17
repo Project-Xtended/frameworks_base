fda4d86ecaa6f5e76349db2d84729c708339e6e9
maxwen <max.weninger@gmail.com>
2018-08-17 16:04:35 +0200

    base: SystemUI: enable tuner & remove from settings
    Change-Id: I4d3ff3ca989e6a6952419d147208318ee269b9d7
    Signed-off-by: xyyx <xyyx@mail.ru>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -95,6 +95,7 @@ public class TunerServiceImpl extends TunerService {
             }
         };
         mUserTracker.startTracking();
+        setTunerEnabled(mContext, true);
     }
 
     @Override
