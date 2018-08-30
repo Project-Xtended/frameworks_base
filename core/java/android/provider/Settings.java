94826372b734f21bcf24ed3eb8eb6d67e3402b79
maxwen <max.weninger@gmail.com>
2018-08-30 22:21:34 +0200

    [1/3] base: SystemUI: keyguard weather view
    Change-Id: Idbedea20d5487a42849c225e3f500aa1f0282c6c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6079,6 +6079,11 @@ public final class Settings {
         public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
 
         /**
+         * @hide
+         */
+        public static final String OMNI_LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6260,6 +6265,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(BUTTON_BRIGHTNESS);
             PRIVATE_SETTINGS.add(BUTTON_BACKLIGHT_ONLY_WHEN_PRESSED);
             PRIVATE_SETTINGS.add(OMNIJAWS_WEATHER_ICON_PACK);
+            PRIVATE_SETTINGS.add(OMNI_LOCKSCREEN_WEATHER_ENABLED);
         }
 
         /**
--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -308,7 +308,7 @@ public class QSFactoryImpl implements QSFactory {
             case "hwkeys":
                 return mHWKeysTileProvider.get();
             case "weather":
-                return mWeatherTile.get();
+                return mWeatherTileProvider.get();
         }
 
         // Custom tiles
