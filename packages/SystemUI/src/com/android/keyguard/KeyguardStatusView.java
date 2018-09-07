e4ad69ae66c6c43e743763c8e470c68092fbc75f
maxwen <max.weninger@gmail.com>
2018-09-07 11:47:40 +0200

    base: SystemUI: keyguard weather view - fix initial kick off after boot
    Change-Id: I64c5082ed675549d67728bcd7a8e19c09c7a21e7

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -120,6 +120,7 @@ public class KeyguardStatusView extends GridLayout implements
             refreshFormat();
             updateOwnerInfo();
             updateLogoutView();
+            updateSettings();
         }
 
         @Override
@@ -210,6 +211,7 @@ public class KeyguardStatusView extends GridLayout implements
         updateOwnerInfo();
         updateLogoutView();
         updateDark();
+        updateSettings();
     }
 
     /**
