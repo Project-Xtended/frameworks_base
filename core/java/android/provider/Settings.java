e3adb751ea089093055e0737f8760bb7a82a8bc5
SuperDroidBond <superdroidbond@yahoo.com>
2018-09-20 15:10:37 +0530

    Xtended Statusbar Clock Customization [1/2] Add Clock size, color & font style.
    Port to Pie + Added Color for Clocks - SuperDroidBond
    Clock fonts and size - @temasek
    Port to Oreo by @varund7726

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4801,6 +4801,24 @@ public final class Settings {
         public static final Validator STATUSBAR_CLOCK_DATE_FORMAT_VALIDATOR = ANY_STRING_VALIDATOR;
 
         /**
+         * Clock font size
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SIZE = "status_bar_clock_size";
+
+        /**
+         * Clock font color
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_COLOR = "status_bar_clock_color";
+
+        /**
+         * Clock font style
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_FONT_STYLE = "status_bar_clock_font_style";
+
+        /**
          * Indicates whether ANBI (Accidental navigation button interaction) is enabled.
          *
          * @hide
