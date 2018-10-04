4956c3b78dbac5c70dfda4375701da7502289999
aviraxp <416810799@qq.com>
2018-10-04 05:38:19 +0000

    ViewRootImpl: break out the loop when NPE caught
    * current gestures implementation here may lead to system hang
    
    Change-Id: Ia32e92de66f8c2d43e9dd73e604c2bd258f5948b

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -4297,11 +4297,10 @@ public final class ViewRootImpl implements ViewParent,
                     int k = 0;
                     while (k < ViewRootImpl.this.mBackupEventList.size()) {
                         try {
-                            boolean ishandled = ViewRootImpl.this.mView.dispatchPointerEvent(
-                                    (MotionEvent) ViewRootImpl.this.mBackupEventList.get(k));
+                            boolean ishandled = ViewRootImpl.this.mView.dispatchPointerEvent((MotionEvent) ViewRootImpl.this.mBackupEventList.get(k));
                             k++;
                         } catch (NullPointerException e) {
-                            Log.e(ViewRootImpl.TAG, "mView does not exist, discard points. " + e);
+                            Log.e(ViewRootImpl.TAG, "mView does not exist, so discard the remaining points. " + e);
                             break;
                         }
                     }
