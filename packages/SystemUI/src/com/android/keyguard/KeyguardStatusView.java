4b476d7af61d9ee6aa2781018fe930dbb0793937
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-21 14:01:48 +0530

    Lock Clock & Date Font Size [1/2]
    Port to O -> P -> Q
    
    **SDB: Remove some sizes & move clock font size to KeygaurdClockSwitch

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -106,6 +106,8 @@ public class KeyguardStatusView extends GridLayout implements
                 updateSettings();
                 mClockView.refreshLockFont();
 		refreshLockDateFont();
+		mClockView.refreshclocksize();
+		mKeyguardSlice.refreshdatesize();
             }
         }
 
@@ -127,6 +129,9 @@ public class KeyguardStatusView extends GridLayout implements
             updateSettings();
             mClockView.refreshLockFont();
             refreshLockDateFont();
+	    refreshLockDateFont();
+	    mClockView.refreshclocksize();
+	    mKeyguardSlice.refreshdatesize();
         }
 
         @Override
@@ -209,6 +214,8 @@ public class KeyguardStatusView extends GridLayout implements
         updateSettings();
         mClockView.refreshLockFont();
 	refreshLockDateFont();
+        mClockView.refreshclocksize();
+	mKeyguardSlice.refreshdatesize();
 
         mTextColor = mClockView.getCurrentTextColor();
 
@@ -254,8 +261,7 @@ public class KeyguardStatusView extends GridLayout implements
     @Override
     public void onDensityOrFontScaleChanged() {
         if (mClockView != null) {
-            mClockView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
-                    getResources().getDimensionPixelSize(R.dimen.widget_big_font_size));
+            mClockView.refreshclocksize();
         }
         if (mOwnerInfo != null) {
             mOwnerInfo.setTextSize(TypedValue.COMPLEX_UNIT_PX,
@@ -297,10 +303,6 @@ public class KeyguardStatusView extends GridLayout implements
             return 0;
         }
         return mLogoutView.getVisibility() == VISIBLE ? mLogoutView.getHeight() : 0;
-    }
-
-    public float getClockTextSize() {
-        return mClockView.getTextSize();
     }
 
     private void refreshLockDateFont() {
@@ -415,6 +417,10 @@ public class KeyguardStatusView extends GridLayout implements
 	if (lockDateFont == 35) {
             mKeyguardSlice.setViewsTypeface(Typeface.create("phantombold-sys", Typeface.NORMAL));
         }
+    }
+
+    public float getClockTextSize() {
+        return mClockView.getTextSize();
     }
 
     /**
