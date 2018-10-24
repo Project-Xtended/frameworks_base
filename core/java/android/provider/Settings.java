7e71674fd9bfdf7ce2e4966f8263ee28e12af3da
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-25 00:43:29 +0530

    LS OwnerInfo Fonts & Size Options [1/2] SuperdroidBond:- It's done on the basis of LS Date Font & Size. Port to O -> P -> Q

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5426,6 +5426,20 @@ public final class Settings {
          */
         public static final String LOCK_SCREEN_WEATHER_ICON_COLOR = "lock_screen_weather_icon_color";
 
+	/**
+	 ** Change fonts for the system lockscreen Owner info
+	 **
+	 ** @hide
+	 **/
+        public static final String LOCK_OWNERINFO_FONTS = "lock_ownerinfo_fonts";
+
+        /**
+	 ** Change font size for the system lockscreen owner
+	 **
+	 ** @hide
+	 **/
+        public static final String LOCKOWNER_FONT_SIZE= "lockowner_font_size";
+
         /**
 	 ** Change fonts for the system lockscreen weather temperature
 	 **
