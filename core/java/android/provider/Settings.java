1353b3645ecbff8b018241c47c2b902ea5d6137b
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-27 18:17:01 +0530

    Add Toggle for LockWeather Image [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4891,6 +4891,14 @@ public final class Settings {
         /** @hide */
         private static final Validator LOCKSCREEN_WEATHER_SHOW_CITY_VALIDATOR = BOOLEAN_VALIDATOR;
 
+       /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_SHOW_IMAGE = "lockscreen_weather_show_image";
+
+        /** @hide */
+        private static final Validator LOCKSCREEN_WEATHER_SHOW_IMAGE_VALIDATOR = BOOLEAN_VALIDATOR;
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
@@ -4978,7 +4986,8 @@ public final class Settings {
             OMNI_LOCKSCREEN_WEATHER_ENABLED,
             AICP_LOCKSCREEN_WEATHER_STYLE,
             LOCKSCREEN_WEATHER_SHOW_TEMP,
-            LOCKSCREEN_WEATHER_SHOW_CITY
+            LOCKSCREEN_WEATHER_SHOW_CITY,
+	    LOCKSCREEN_WEATHER_SHOW_IMAGE,
         };
 
         /**
@@ -5131,6 +5140,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(AICP_LOCKSCREEN_WEATHER_STYLE);
             PRIVATE_SETTINGS.add(LOCKSCREEN_WEATHER_SHOW_TEMP);
             PRIVATE_SETTINGS.add(LOCKSCREEN_WEATHER_SHOW_CITY);
+	    PRIVATE_SETTINGS.add(LOCKSCREEN_WEATHER_SHOW_IMAGE);
         }
 
         /**
@@ -5250,6 +5260,7 @@ public final class Settings {
             VALIDATORS.put(AICP_LOCKSCREEN_WEATHER_STYLE,AICP_LOCKSCREEN_WEATHER_STYLE_VALIDATOR);
             VALIDATORS.put(LOCKSCREEN_WEATHER_SHOW_TEMP, LOCKSCREEN_WEATHER_SHOW_TEMP_VALIDATOR);
             VALIDATORS.put(LOCKSCREEN_WEATHER_SHOW_CITY, LOCKSCREEN_WEATHER_SHOW_CITY_VALIDATOR);
+	    VALIDATORS.put(LOCKSCREEN_WEATHER_SHOW_IMAGE, LOCKSCREEN_WEATHER_SHOW_IMAGE_VALIDATOR);
         }
 
         /**
