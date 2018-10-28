91966e7f74240b1c256023098b27bd9dbc83fb83
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-28 13:13:37 +0530

    LS Weather Temp & City Fonts [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5760,6 +5760,20 @@ public final class Settings {
         public static final String ANBI_ENABLED_OPTION = "anbi_enabled_option";
 
         /**
+	 ** Change fonts for the system lockscreen weather temperature
+	 **
+	 ** @hide
+	 **/
+         public static final String LOCK_WEATHER_TEMP_FONTS = "lock_weather_temp_fonts";
+
+        /**
+	 ** Change fonts for the system lockscreen weather city
+	 **
+	 ** @hide
+	 **/
+         public static final String LOCK_WEATHER_CITY_FONTS = "lock_weather_city_fonts";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
