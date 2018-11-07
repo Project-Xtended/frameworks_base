653c8001e55a91c4e3210cbb96ec37a29fad03bb
varund7726 <date.varun123@gmail.com>
2018-11-07 13:23:32 -0700

    Pulse: allow setting fading blocks opacity [1/3]
     Please enter the commit message for your changes. Lines starting
    
    Change-Id: I7c4a0aa41672a6c6ed37888c7745f91383662360

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10817,6 +10817,12 @@ public final class Settings {
         public static final String PULSE_APPS_BLACKLIST = "pulse_apps_blacklist";
 
         /**
+         * Pulse Fading units opacity
+         * @hide
+         */
+        public static final String PULSE_FADING_BLOCKS_OPACITY = "pulse_fading_blocks_opacity";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
