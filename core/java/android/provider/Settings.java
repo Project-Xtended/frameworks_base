f5e4199c561f5f40004d8a134a7d5a51806d98ff
Anushek Prasal <anushekprasal@gmail.com>
2018-11-12 01:10:05 +0530

    base: Add QS header styles [1/3]
    Gives the user a choice between black, dark grey, light grey and accent based options.
    
    Based on the code of Fruity pebbles and QS tile picker.
    
    @Aston-Martinn | Forward port to Q
    
    Change-Id: Ibe9d0e6c69a42df85a0e37f4d272440be1c07a40

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6893,6 +6893,11 @@ public final class Settings {
         public static final String FOD_ANIM = "fod_anim";
 
         /**
+         * @hide
+         */
+        public static final String QS_HEADER_STYLE = "qs_header_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -7301,6 +7306,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(HIDE_SMART_REPLIES);
             PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER_HEIGHT);
             PRIVATE_SETTINGS.add(QS_DATAUSAGE);
+            PRIVATE_SETTINGS.add(QS_HEADER_STYLE);
         }
 
         /**
