a15f396df51692cdb8181746d0b4a78597e7fea4
Alex Cruz <alex@dirtyunicorns.com>
2018-11-17 19:41:05 +0530

    Power Menu animations [1/2]
    SDB: Bring it back from the Dead & adapt for A12
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5260,6 +5260,13 @@ public final class Settings {
         public static final String LOCKSCREEN_TIMEOUT = "lockscreen_timeout";
 
         /**
+         * Whether to display set of animations when prompting the power menu
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
