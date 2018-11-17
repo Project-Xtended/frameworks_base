4058655c54e1555d8e7a51742a7c3afc3c8edd5e
Alex Cruz <alex@dirtyunicorns.com>
2018-11-17 19:41:05 +0530

    Power Menu animations [1/2]
    SDB: Bring it back from the Dead

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6143,6 +6143,13 @@ public final class Settings {
         public static final String USE_EDGE_SERVICE_FOR_GESTURES = "edge_service_for_gestures";
 
         /**
+         * Whether to display set of animations when prompting the power menu
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
