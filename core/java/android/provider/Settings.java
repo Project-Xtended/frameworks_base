c196eb68d53fe61def027a57716aa1717487ddcc
bigrushdog <randall.rushing@gmail.com>
2018-11-21 20:03:32 +0100

    bringup: Fontservice

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5727,6 +5727,20 @@ public final class Settings {
         public static final String QS_ROWS_LANDSCAPE = "qs_rows_landscape";
 
         /**
+         * Hold current font name and name of the originating package Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_INFO = "font_info";
+        
+        /**
+         * Hold list of installed font provider packages. Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_PACKAGES = "font_packages";
+
+        /**
          * How many columns to show in the qs panel when in portrait
          * @hide
          */
