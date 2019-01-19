9881953e8607d5898b4357431aaf75665a6be516
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 17:30:24 -0600

    XtendedUtils: Add method to determine battery temp
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/com/android/internal/util/xtended/XtendedUtils.java
+++ core/java/com/android/internal/util/xtended/XtendedUtils.java
@@ -48,7 +48,10 @@ import android.hardware.camera2.CameraManager;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.Uri;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -123,6 +126,20 @@ public class XtendedUtils {
         }
     }
 
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
+    }
+
+    // Check to see if a package is installed
     public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
         if (pkg != null) {
             try {
