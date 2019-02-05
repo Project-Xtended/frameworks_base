2bd1cb39335b4d09a2a75ed5bcc975d210ae22d4
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-02-05 21:22:54 +0000

    [OP Gestures] Enable gestures on the keyguard as well
    Change-Id: I9965f2f465c6f2ecbf5f4c9536681948c7432302
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- services/core/java/com/android/server/policy/GestureButton.java
+++ services/core/java/com/android/server/policy/GestureButton.java
@@ -60,7 +60,6 @@ public class GestureButton implements PointerEventListener {
     private long mDownTime;
     private float mFromX;
     private float mFromY;
-    private boolean mIsKeyguardShowing;
     private float mLastX;
     private float mLastY;
     private int mNavigationBarPosition = 0;
@@ -149,11 +148,6 @@ public class GestureButton implements PointerEventListener {
             float rawY = event.getRawY();
             switch (action) {
                 case MotionEvent.ACTION_DOWN:
-                    mIsKeyguardShowing = mPwm.isKeyguardShowingAndNotOccluded();
-                    if (mIsKeyguardShowing) {
-                        break;
-                    }
-
                     mPreparedKeycode = -1;
                     if (mNavigationBarPosition == 0) {
                         if (rawY >= ((float) (mScreenHeight - mSwipeStartThreshold))) {
@@ -186,7 +180,7 @@ public class GestureButton implements PointerEventListener {
                     }
                     break;
                 case MotionEvent.ACTION_UP:
-                    if (!mIsKeyguardShowing && mPreparedKeycode != -1) {
+                    if (mPreparedKeycode != -1) {
                         if (DEBUG)
                             Slog.i(TAG, "ACTION_UP " + mPreparedKeycode + " " + mRecentsTriggered + " " + mKeyEventHandled + " " + mLongSwipePossible);
                         mGestureButtonHandler.removeMessages(MSG_SEND_SWITCH_KEY);
@@ -208,7 +202,7 @@ public class GestureButton implements PointerEventListener {
                     }
                     break;
                 case MotionEvent.ACTION_MOVE:
-                    if (!mKeyEventHandled && !mRecentsTriggered && !mIsKeyguardShowing && mPreparedKeycode != -1) {
+                    if (!mKeyEventHandled && !mRecentsTriggered && mPreparedKeycode != -1) {
                         float moveDistanceSinceDown;
                         if (mNavigationBarPosition == 0) {
                             moveDistanceSinceDown = Math.abs(mFromY - rawY);
