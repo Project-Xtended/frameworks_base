95471a0303f09bd2985642998d637076ed27cd08
maxwen <max.weninger@gmail.com>
2019-02-16 12:40:11 +0100

    base: SystemUI: fix lock screen wallpaper usage
    Change-Id: Ic24bdbe104b5dc81bdf379a01bff572a87613551

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2027,14 +2027,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     @Override
     public void updateMediaMetaData(boolean metaDataChanged, boolean allowEnterAnimation) {
         Trace.beginSection("StatusBar#updateMediaMetaData");
-
         // ensure visualizer is visible regardless of artwork
         mMediaManager.setMediaPlaying();
-
-        if (!SHOW_LOCKSCREEN_MEDIA_ARTWORK) {
-            Trace.endSection();
-            return;
-        }
 
         if (mBackdrop == null) {
             Trace.endSection();
@@ -2060,7 +2054,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
 
         Drawable artworkDrawable = null;
-        if (mediaMetadata != null && mLockscreenMediaMetadata) {
+        if (mediaMetadata != null && SHOW_LOCKSCREEN_MEDIA_ARTWORK && mLockscreenMediaMetadata) {
             Bitmap artworkBitmap = mediaMetadata.getBitmap(MediaMetadata.METADATA_KEY_ART);
             if (artworkBitmap == null) {
                 artworkBitmap = mediaMetadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART);
