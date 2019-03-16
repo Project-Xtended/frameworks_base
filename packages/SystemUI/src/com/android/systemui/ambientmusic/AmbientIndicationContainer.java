ABC-KANG/abc_frameworks_base

Port ambient Now Playing container from Pixel [stripped down version]
ezio84: this is a very stripped down part of code from the original commit by Joe:
"SystemUI: Port Now Playing feature from Pixel [1/2]"
https://review.carbonrom.org/#/c/4950/
adapted to be called from StatusBar class for our local Ambient Display
music ticker.
Removed everything we don't need, services and listeners, and reparsed
everything to make it clean.

Change-Id: If1c2cf233d72ed8b31bc44f9852ba7b5c1181697

packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
@@ -1,0 +1,65 @@
+package com.android.systemui.ambientmusic;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.systemui.AutoReinflateContainer;
+import com.android.systemui.doze.DozeReceiver;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.StatusBar;
+
+import com.android.systemui.ambientmusic.AmbientIndicationInflateListener;
+
+public class AmbientIndicationContainer extends AutoReinflateContainer implements DozeReceiver {
+    private View mAmbientIndication;
+    private boolean mDozing;
+    private ImageView mIcon;
+    private CharSequence mIndication;
+    private StatusBar mStatusBar;
+    private TextView mText;
+    private Context mContext;
+
+    public AmbientIndicationContainer(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        mContext = context;
+    }
+
+    public void hideIndication() {
+        setIndication(null);
+    }
+
+    public void initializeView(StatusBar statusBar) {
+        mStatusBar = statusBar;
+        addInflateListener(new AmbientIndicationInflateListener(this));
+    }
+
+    public void updateAmbientIndicationView(View view) {
+        mAmbientIndication = findViewById(R.id.ambient_indication);
+        mText = (TextView)findViewById(R.id.ambient_indication_text);
+        mIcon = (ImageView)findViewById(R.id.ambient_indication_icon);
+        setIndication(mIndication);
+    }
+
+    @Override
+    public void setDozing(boolean dozing) {
+        mDozing = dozing;
+    }
+
+    public void setIndication(CharSequence charSequence) {
+        mText.setText(charSequence);
+        mIndication = charSequence;
+        mAmbientIndication.setClickable(false);
+        boolean infoAvaillable = TextUtils.isEmpty((CharSequence)charSequence);
+        if (infoAvaillable) {
+            mAmbientIndication.setVisibility(View.INVISIBLE);
+        } else {
+            mAmbientIndication.setVisibility(View.VISIBLE);
+        }
+    }
+}
+

Project-Xtended/frameworks_base

packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
@@ -1,0 +1,69 @@
+package com.android.systemui.ambientmusic;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.systemui.AutoReinflateContainer;
+import com.android.systemui.doze.DozeReceiver;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.StatusBar;
+
+import aoscp.support.lottie.LottieAnimationView;
+
+import com.android.systemui.ambientmusic.AmbientIndicationInflateListener;
+
+public class AmbientIndicationContainer extends AutoReinflateContainer implements DozeReceiver {
+    private View mAmbientIndication;
+    private boolean mDozing;
+    private LottieAnimationView mIcon;
+    private CharSequence mIndication;
+    private StatusBar mStatusBar;
+    private TextView mText;
+    private Context mContext;
+
+    public AmbientIndicationContainer(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        mContext = context;
+    }
+
+    public void hideIndication() {
+        setIndication(null);
+    }
+
+    public void initializeView(StatusBar statusBar) {
+        mStatusBar = statusBar;
+        addInflateListener(new AmbientIndicationInflateListener(this));
+    }
+
+    public void updateAmbientIndicationView(View view) {
+        mAmbientIndication = findViewById(R.id.ambient_indication);
+        mText = (TextView)findViewById(R.id.ambient_indication_text);
+        mIcon = (LottieAnimationView)findViewById(R.id.ambient_indication_icon);
+        setIndication(mIndication);
+    }
+
+    @Override
+    public void setDozing(boolean dozing) {
+        mDozing = dozing;
+    }
+
+    public void setIndication(CharSequence charSequence) {
+        mText.setText(charSequence);
+        mIndication = charSequence;
+	mIcon.setAnimation(R.raw.ambient_music_note);
+	mIcon.playAnimation();
+        mAmbientIndication.setClickable(false);
+	boolean infoAvaillable = TextUtils.isEmpty((CharSequence)charSequence);
+        if (infoAvaillable) {
+            mAmbientIndication.setVisibility(View.INVISIBLE);
+        } else {
+            mAmbientIndication.setVisibility(View.VISIBLE);
+        }
+    }
+}
+