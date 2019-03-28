ebff30ea100079ddd05ba87079fcce13716bc768
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    ZeNiXxX - Bring to Android 10

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5373,6 +5373,16 @@ public final class Settings {
         public static final String XTENSIONS_STYLE = "xtensions_style";
 
         /**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
