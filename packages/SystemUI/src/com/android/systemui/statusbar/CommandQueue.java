5f7b9ec847229f4111d165b6ecca9381f0a0efe4
Peter Cai <peter@typeblog.net>
2019-04-05 16:44:18 +0200

    Initial support for in-display fingerprint sensors
    * Initially based upon phh's Mi9 implementation,
      however a lot of changes have been made since.
    * All device specific stuff has been moved to
      IFingerprintInscreen HIDL interface and most
      of code has been improved and refactored.
    
    AOSiP edit: remove all ties to LineageContextConstants.Features.FOD as we dont want no damn sdk stuffs
    
    Change-Id: Ia905ee8b2e828e0d34ef8c01a41717323dcc220f
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -119,6 +119,8 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     private static final int MSG_RESTART_UI                    = 48 << MSG_SHIFT;
     private static final int MSG_TOGGLE_CAMERA_FLASH           = 49 << MSG_SHIFT;
     private static final int MSG_TOGGLE_CAMERA_FLASH_STATE     = 59 << MSG_SHIFT;
+    private static final int MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW = 51 << MSG_SHIFT;
+    private static final int MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW = 52 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -280,8 +282,10 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
         default void onBiometricHelp(String message) { }
         default void onBiometricError(String error) { }
         default void hideBiometricDialog() { }
-	default void restartUI() { }
-	default void toggleCameraFlash() { }
+    	default void restartUI() { }
+	    default void toggleCameraFlash() { }
+        default void showInDisplayFingerprintView() { }
+        default void hideInDisplayFingerprintView() { }
 
         /**
          * @see IStatusBar#onDisplayReady(int)
@@ -792,6 +796,20 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     }
 
     @Override
+    public void showInDisplayFingerprintView() {
+        synchronized (mLock) {
+            mHandler.obtainMessage(MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW).sendToTarget();
+        }
+    }
+
+    @Override
+    public void hideInDisplayFingerprintView() {
+        synchronized (mLock) {
+            mHandler.obtainMessage(MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW).sendToTarget();
+        }
+    }
+
+    @Override
     public void onDisplayReady(int displayId) {
         synchronized (mLock) {
             mHandler.obtainMessage(MSG_DISPLAY_READY, displayId, 0).sendToTarget();
@@ -1132,6 +1150,16 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
                 case MSG_TOGGLE_CAMERA_FLASH_STATE:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).toggleCameraFlashState(msg.arg1 != 0);
+                    }
+                    break;
+                case MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).showInDisplayFingerprintView();
+                    }
+                    break;
+                case MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).hideInDisplayFingerprintView();
                     }
                     break;
             }
