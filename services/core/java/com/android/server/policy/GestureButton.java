8b01f492e56ddca8fa0586b11d43e1562ab71fe1
Marko Man <darkobas@gmail.com>
2019-07-10 12:03:24 +0200

    [1/2] Op gestures: add swipe start height setting
    aosip edit: remove the omni
    
    Change-Id: I9e0b9db44f402be106806b1c45a50a75432f8cf1
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- services/core/java/com/android/server/policy/GestureButton.java
+++ services/core/java/com/android/server/policy/GestureButton.java
@@ -71,6 +71,7 @@ public class GestureButton implements PointerEventListener {
     private int mScreenHeight = -1;
     private int mScreenWidth = -1;
     private boolean mSwipeStartFromEdge;
+    private int mSwipeStartThreshold;
     private boolean mKeyEventHandled;
     private boolean mRecentsTriggered;
     private boolean mLongSwipePossible;
@@ -92,9 +93,6 @@ public class GestureButton implements PointerEventListener {
             vibrator.vibrate(VibrationEffect.createOneShot(mHapticDuration, VibrationEffect.DEFAULT_AMPLITUDE));
        }
     };
-
-    private final int mSwipeStartThreshold =
-                    SystemProperties.getInt("ro.bottom_gesture.swipe_start.threshold", 20);
 
     private class GestureButtonHandler extends Handler {
 
@@ -136,6 +134,7 @@ public class GestureButton implements PointerEventListener {
         windowManager.getDefaultDisplay().getRealMetrics(displayMetrics);
         mScreenHeight = Math.max(displayMetrics.widthPixels, displayMetrics.heightPixels);
         mScreenWidth = Math.min(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        mSwipeStartThreshold = context.getResources().getInteger(R.integer.nav_gesture_swipe_start);
         mSwipeMinLength = getSwipeLengthInPixel(context.getResources().getInteger(R.integer.nav_gesture_swipe_min_length));
         mMoveTolerance = context.getResources().getInteger(R.integer.nav_gesture_move_threshold);
         mSwipeTriggerTimeout  = context.getResources().getInteger(R.integer.nav_gesture_swipe_timout);
@@ -334,6 +333,11 @@ public class GestureButton implements PointerEventListener {
         mHapticDuration = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.BOTTOM_GESTURE_FEEDBACK_DURATION, 50,
                 UserHandle.USER_CURRENT);
+        mSwipeStartThreshold = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.BOTTOM_GESTURE_SWIPE_START,
+                getSwipeLengthInPixel(mContext.getResources().getInteger(R.integer.nav_gesture_swipe_start)),
+                UserHandle.USER_CURRENT);
+
         if (DEBUG) Slog.i(TAG, "updateSettings mSwipeTriggerTimeout = " + mSwipeTriggerTimeout + " mSwipeMinLength = " + mSwipeMinLength);
     }
 
