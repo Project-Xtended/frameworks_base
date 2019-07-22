1bb8ca10e984a449bafefc53b5539ba436bd71eb
Weijie Wang <weijiew@codeaurora.org>
2019-07-22 19:36:13 +0800

    SystemUI: Update the media notification only when needed
    The media notification is updated every second even if the
    screen is turned off, which causes unnecessary power consumption.
    So update the media notification only when needed
    
    Change-Id: I4d5bcc56fe804754eebec92d83397dac354ec012
    CRs-Fixed: 2493218
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/wrapper/NotificationMediaTemplateViewWrapper.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/wrapper/NotificationMediaTemplateViewWrapper.java
@@ -32,6 +32,7 @@ import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewStub;
+import android.view.ViewTreeObserver;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -252,15 +253,18 @@ public class NotificationMediaTemplateViewWrapper extends NotificationTemplateVi
 
     private void startTimer() {
         clearTimer();
-        if (mIsViewVisible) {
-            mSeekBarTimer = new Timer(true /* isDaemon */);
-            mSeekBarTimer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    mHandler.post(mOnUpdateTimerTick);
-                }
-            }, 0, PROGRESS_UPDATE_INTERVAL);
-        }
+        updateSeekBarView();
+    }
+
+    private void updateSeekBarView() {
+        mSeekBarView.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener(){
+            @Override
+            public boolean onPreDraw(){
+                mSeekBarView.getViewTreeObserver().removeOnPreDrawListener(this);
+                mHandler.postDelayed(mOnUpdateTimerTick,PROGRESS_UPDATE_INTERVAL);
+                return true;
+            }
+        });
     }
 
     private void clearTimer() {
@@ -318,6 +322,7 @@ public class NotificationMediaTemplateViewWrapper extends NotificationTemplateVi
         public void run() {
             if (mMediaController != null && mSeekBar != null) {
                 PlaybackState playbackState = mMediaController.getPlaybackState();
+                updateSeekBarView();
                 if (playbackState != null) {
                     updatePlaybackUi(playbackState);
                 } else {
