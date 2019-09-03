74428671c99db6e1c1cd4c52ee7f739a17cda132
SuperDroidBond <superdroidbond@yahoo.com>
2019-09-04 00:21:46 +0530

    remove ScreenrecordTile

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -56,7 +56,6 @@ import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.PieTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenStabilizationTile;
-import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SleepScreenTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
@@ -149,8 +148,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new CompassTile(mHost);
             case "reboot":
                 return new RebootTile(mHost);
-            case "screenrecord":
-                return new ScreenrecordTile(mHost);
             case "expanded_desktop":
                 return new ExpandedDesktopTile(mHost);
             case "usb_tether":
