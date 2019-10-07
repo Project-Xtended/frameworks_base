cfb70b31194cf7e4cf8128804df8038a2cb15cd1
darkobas <darkobas@gmail.com>
2019-10-07 20:31:48 +0200

    base: add option to enable AOD on charging only [1/2]
    @idoybh edits: Adapted to A11's settings backup
    
    Change-Id: I831583fde68de15788e3d7ecab55d864726d140f

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -85,5 +85,6 @@ public class SystemSettings {
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.COLOR_BUCKET_OVERLAY,
+        Settings.System.DOZE_ON_CHARGE,
     };
 }
