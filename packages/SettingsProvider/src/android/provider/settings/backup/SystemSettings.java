ae24fe0718410acbfe6ab346f52b405612897584
Pranav Vashi <neobuddy89@gmail.com>
2019-10-08 22:59:24 +0530

    base: Add tunables for navbar layout [1/2]
    idoybh:
    * Back it up properly
    * There is no use in it being under secure, system it is
    * Get rid of useless global vars
    * Use existing settings observer instead of tunables
    * Rewrote observing to match A12's way
    
    Change-Id: I95c7427312400540f840c48b9456e0fe71f74e5c
    
    also a squash of:
    
    base: Make a proper validator for navbar layouts
    
    Change-Id: Ia4a8ad42f8fcc1aeeaf1d4b42a5fda98aab2e42f
    
    NavigationBarInflaterView: Apply custom layouts on boot
    
    Change-Id: I0b9f85198b705c3f8140097c18988069bd965997
    
    NavigationBarInfalterView: Don't apply a custom layout when using gestural
    
    This causes gestural navbar to be gone and causes various other issues
    when it happens
    
    Change-Id: I56cfec66d551fb5bdcf37e82cfd91588a52cd988
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -92,5 +92,6 @@ public class SystemSettings {
         Settings.System.COLOR_BUCKET_OVERLAY,
         Settings.System.USE_OLD_MOBILETYPE,
         Settings.System.NAVIGATION_BAR_INVERSE,
+        Settings.System.NAVBAR_LAYOUT_VIEWS,
     };
 }
