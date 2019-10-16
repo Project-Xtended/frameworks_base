7ef653785a2d109184abb660b759fe08deb524fb
ezio84 <brabus84@gmail.com>
2019-10-17 00:55:29 +0530

    Ambient brightness values: use a custom system setting [1/2]
    *part 2 is in CustomDoze package

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5600,6 +5600,13 @@ public final class Settings {
         public static final String OMNI_DOZE_BRIGHTNESS = "doze_brightness";
 
         /**
+         * Ambient screen settings
+         * "screen_brightness_array=0:1:2:3:4"
+         * @hide
+         */
+        public static final String ALWAYS_ON_DISPLAY_CONSTANTS_CUST = "always_on_display_constants_cust";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
