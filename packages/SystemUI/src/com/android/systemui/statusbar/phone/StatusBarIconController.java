46fd0571e2925b4fe0f00ce591537a2c5634d92c
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: bring back old style mobile data indicators
    TODO maybe add settings observer to do update faster then
    waiting for the next update

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -24,7 +24,10 @@ import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_WIFI
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_IMS;
 
 import android.content.Context;
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.ArraySet;
 import android.view.Gravity;
@@ -310,7 +313,7 @@ public interface StatusBarIconController {
         @VisibleForTesting
         protected StatusBarMobileView addMobileIcon(int index, String slot, MobileIconState state) {
             StatusBarMobileView view = onCreateStatusBarMobileView(slot);
-            view.applyMobileState(state);
+            view.applyMobileState(state, useOldStyleMobileDataIcons());
             mGroup.addView(view, index, onCreateLayoutParams());
 
             if (mIsInDemoMode) {
@@ -435,7 +438,7 @@ public interface StatusBarIconController {
         public void onSetMobileIcon(int viewIndex, MobileIconState state) {
             StatusBarMobileView view = (StatusBarMobileView) mGroup.getChildAt(viewIndex);
             if (view != null) {
-                view.applyMobileState(state);
+                view.applyMobileState(state, useOldStyleMobileDataIcons());
             }
 
             if (mIsInDemoMode) {
@@ -478,6 +481,12 @@ public interface StatusBarIconController {
 
         protected DemoStatusIcons createDemoStatusIcons() {
             return new DemoStatusIcons((LinearLayout) mGroup, mIconSize);
+        }
+
+        private boolean useOldStyleMobileDataIcons() {
+            return Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.USE_OLD_MOBILETYPE, 0,
+                    UserHandle.USER_CURRENT) != 0;
         }
     }
 }
