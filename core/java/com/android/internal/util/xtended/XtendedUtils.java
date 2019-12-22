3553d1875f7ad0ac1386d7c4ba0e310c6116efe5
Alex Cruz <du.alexcruz@gmail.com>
2019-12-22 14:39:55 -0500

    XtendedUtils: Add method to detect whether overlay is enabled
    Change-Id: Ibd4ecb00ceed29604fdcd5d2822edcdb3ac399e2
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/com/android/internal/util/xtended/XtendedUtils.java
+++ core/java/com/android/internal/util/xtended/XtendedUtils.java
@@ -17,10 +17,40 @@
 package com.android.internal.util.xtended;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.om.IOverlayManager;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
+import android.content.om.OverlayInfo;
 import android.content.pm.PackageManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemClock;
+import android.os.SystemProperties;
+import android.os.Vibrator;
+import android.os.UserHandle;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.IWindowManager;
+import android.provider.MediaStore;
+import android.view.InputDevice;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.android.internal.statusbar.IStatusBarService;
 
@@ -28,6 +58,41 @@ import com.android.internal.statusbar.IStatusBarService;
  * Some custom utilities
  */
 public class XtendedUtils {
+
+    public static final String ANDROIDNS = "http://schemas.android.com/apk/res/android";
+    public static final String PACKAGE_SYSTEMUI = "com.android.systemui";
+    public static final String PACKAGE_ANDROID = "android";
+    public static final String FORMAT_NONE = "none";
+    public static final String FORMAT_FLOAT = "float";
+    public static final String ID = "id";
+    public static final String DIMEN = "dimen";
+    public static final String DIMEN_PIXEL = "dimen_pixel";
+    public static final String FLOAT = "float";
+    public static final String INT = "integer";
+    public static final String DRAWABLE = "drawable";
+    public static final String COLOR = "color";
+    public static final String BOOL = "bool";
+    public static final String STRING = "string";
+    public static final String ANIM = "anim";
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
+
+    private static final String TAG = XtendedUtils.class.getSimpleName();
+
+    private static OverlayManager mOverlayService;
+
+    private static IStatusBarService mStatusBarService = null;
+
+    private static IStatusBarService getStatusBarService() {
+        synchronized (XtendedUtils.class) {
+            if (mStatusBarService == null) {
+                mStatusBarService = IStatusBarService.Stub.asInterface(
+                        ServiceManager.getService("statusbar"));
+            }
+            return mStatusBarService;
+        }
+    }
+
     public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
         if (pkg != null) {
             try {
@@ -57,6 +122,42 @@ public class XtendedUtils {
             } catch (RemoteException e) {
                 // do nothing.
             }
+        }
+    }
+
+    // Method to detect whether an overlay is enabled or not
+    public static boolean isThemeEnabled(String packageName) {
+        mOverlayService = new OverlayManager();
+        try {
+            List<OverlayInfo> infos = mOverlayService.getOverlayInfosForTarget("android",
+                    UserHandle.myUserId());
+            for (int i = 0, size = infos.size(); i < size; i++) {
+                if (infos.get(i).packageName.equals(packageName)) {
+                    return infos.get(i).isEnabled();
+                }
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static class OverlayManager {
+        private final IOverlayManager mService;
+
+        public OverlayManager() {
+            mService = IOverlayManager.Stub.asInterface(
+                    ServiceManager.getService(Context.OVERLAY_SERVICE));
+        }
+
+        public void setEnabled(String pkg, boolean enabled, int userId)
+                throws RemoteException {
+            mService.setEnabled(pkg, enabled, userId);
+        }
+
+        public List<OverlayInfo> getOverlayInfosForTarget(String target, int userId)
+                throws RemoteException {
+            return mService.getOverlayInfosForTarget(target, userId);
         }
     }
 }
