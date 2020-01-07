19d8eadbcd387e1b017147ffca51ceed9c863b6b
maxwen <max.weninger@gmail.com>
2020-01-07 19:50:18 +0100

    SystemUI: custom keyguard clock kick off
    Stripped down version of the original,
    remove one line and two line clock.
    
    allow custom clock to be in the same position as the default
    clock. Add bold hour clock back from the days where we
    had custom keyguard clock already
    
    Change-Id: Ic97603a001dfd61e617f94418fc8edd97fa8c274

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -534,6 +534,14 @@ public class NotificationPanelView extends PanelView implements
         return mKeyguardStatusView.hasCustomClock();
     }
 
+    public KeyguardStatusView getKeyguardStatusView() {
+        return mKeyguardStatusView;
+    }
+
+    public boolean hasCustomClockInBigContainer() {
+        return mKeyguardStatusView.hasCustomClockInBigContainer();
+    }
+
     private void setStatusBar(StatusBar bar) {
         mStatusBar = bar;
         mKeyguardBottomArea.setStatusBar(mStatusBar);
@@ -873,7 +881,7 @@ public class NotificationPanelView extends PanelView implements
                     (int) (mKeyguardStatusView.getHeight()
                             - mShelfHeight / 2.0f - mDarkIconSize / 2.0f),
                     clockPreferredY,
-                    hasCustomClock(),
+                    hasCustomClockInBigContainer(),
                     hasVisibleNotifications,
                     mInterpolatedDarkAmount,
                     mEmptyDragAmount,
