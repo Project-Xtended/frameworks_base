4d8656b32d1a312f4b0e15dd91739f66502b94a1
maxwen <max.weninger@gmail.com>
2020-01-17 23:51:35 +0100

    SystemUI: tuner: rework navigation bar settings
    the layout options is only valid for legacy 3-button mode
    and add a new one to hide the handle in gesture mode
    
    Change-Id: I0ec61eb54cc71b5c91ae43ca23fbf668a7b5aa47

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
@@ -57,6 +57,7 @@ public class NavigationBarInflaterView extends FrameLayout
     public static final String NAV_BAR_LEFT = "sysui_nav_bar_left";
     public static final String NAV_BAR_RIGHT = "sysui_nav_bar_right";
     public static final String NAV_BAR_INVERSE = "sysui_nav_bar_inverse";
+    public static final String NAV_BAR_SHOW_HANDLE = "sysui_nav_bar_show_handle";
 
     public static final String MENU_IME_ROTATE = "menu_ime";
     public static final String BACK = "back";
@@ -104,6 +105,7 @@ public class NavigationBarInflaterView extends FrameLayout
 
     private OverviewProxyService mOverviewProxyService;
     private int mNavBarMode = NAV_BAR_MODE_3BUTTON;
+    private boolean mHideHandle;
 
     private boolean mInverseLayout;
 
@@ -147,7 +149,11 @@ public class NavigationBarInflaterView extends FrameLayout
             String navbarLayout = getContext().getString(showDpadArrowKeys()
                     ? R.string.config_navBarLayoutHandleArrows
                     : R.string.config_navBarLayoutHandle);
-            return navbarLayout;
+            if (mHideHandle) {
+                return navbarLayout.replace(HOME_HANDLE, NAVSPACE);
+            } else {
+                return navbarLayout;
+            }
         } else {
             final int defaultResource = mOverviewProxyService.shouldShowSwipeUpUI()
                             ? R.string.config_navBarLayoutQuickstep
@@ -167,6 +173,7 @@ public class NavigationBarInflaterView extends FrameLayout
         super.onAttachedToWindow();
         Dependency.get(TunerService.class).addTunable(this, NAV_BAR_INVERSE);
         Dependency.get(TunerService.class).addTunable(this, NAV_BAR_VIEWS);
+        Dependency.get(TunerService.class).addTunable(this, NAV_BAR_SHOW_HANDLE);
         Dependency.get(CustomSettingsService.class).addIntObserver(this, Settings.System.NAVIGATION_BAR_ARROW_KEYS);
     }
 
@@ -182,8 +189,14 @@ public class NavigationBarInflaterView extends FrameLayout
     @Override
     public void onTuningChanged(String key, String newValue) {
         if (NAV_BAR_VIEWS.equals(key)) {
-            if (QuickStepContract.isGesturalMode(mNavBarMode)) {
+            if (QuickStepContract.isLegacyMode(mNavBarMode)) {
                 setNavigationBarLayout(newValue);
+            }
+        }
+        if (NAV_BAR_SHOW_HANDLE.equals(key)) {
+            mHideHandle = newValue != null && newValue.equals("0");
+            if (QuickStepContract.isGesturalMode(mNavBarMode)) {
+                onLikelyDefaultLayoutChange();
             }
         }
         if (NAV_BAR_INVERSE.equals(key)) {
