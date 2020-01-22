478ce83e63aff30405ee0306bb28935cbbfb1430
00day0 <therandomuser11@gmail.com>
2020-01-22 16:25:25 +0100

    Fix oxygenos dismiss all appearing when there arent any notifications to dimiss
    Change-Id: I78540dd493265a6c1980753a5b7a116e17074fda
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1383,6 +1383,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     }
 
+    public static void setHasClearableNotifications(boolean state) {
+        mClearableNotifications = state;
+    }
+
     public static void setDismissAllVisible(boolean visible) {
 
         if(mClearableNotifications && mState != StatusBarState.KEYGUARD && visible && isDismissAllButtonEnabled()) {
