e2f72bff1280f40e9bad57c4b641be3f1902697c
00day0 <therandomuser11@gmail.com>
2020-01-28 00:07:58 +0100

    FODCircleView: Recognizing fingerprint animation [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6530,6 +6530,12 @@ public final class Settings {
         public static final String FOD_PRESSED_STATE = "fod_pressed_state";
 
         /**
+         * FOD recognizing animation
+         * @hide
+         */
+        public static final String FOD_RECOGNIZING_ANIMATION = "fod_recognizing_animation";
+
+        /**
          * Which Vibration Pattern to use
          * 0: dzzz-dzzz
          * 1: dzzz-da
