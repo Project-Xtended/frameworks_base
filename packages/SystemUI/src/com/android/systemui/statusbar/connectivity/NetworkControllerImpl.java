8518d26fe60a7e95e04832d84b8a97b60b2f6a84
Lup Gabriel <gwolf2u.website@gmail.com>
2020-02-05 19:48:15 +0200

    Add MIUI 11 VoLTE icon [1/2]
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/NetworkControllerImpl.java
@@ -844,6 +844,10 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 case 5:
                     resId = R.drawable.ic_hd_volte;
                     break;
+                // MemeUi VoLTE
+                case 6:
+                    resId = R.drawable.ic_volte_miui;
+                    break;
                 // Dynamic
                 case 1:
                 default:
@@ -901,7 +905,11 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 case 5:
                     resId = R.drawable.ic_hd_volte;
                     break;
-                // Dynamic
+                // MemeUi VoLTE
+                case 6:
+                    resId = R.drawable.ic_volte_miui;
+                    break;
+                 // Dynamic
                 case 1:
                 default:
                     resId = R.drawable.stat_sys_volte;
@@ -942,6 +950,10 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 case 5:
                     resId = R.drawable.ic_hd_volte;
                     break;
+                // MemeUi VoLTE
+                case 6:
+                    resId = R.drawable.ic_volte_miui;
+                    break;
                 // Dynamic
                 case 1:
                 default:
@@ -966,6 +978,10 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 case 5:
                     resId = R.drawable.ic_hd_volte;
                     break;
+                // MemeUi VoLTE
+                case 6:
+                    resId = R.drawable.ic_volte_miui;
+                    break;
                 // Dynamic
                 case 1:
                 default:
@@ -989,6 +1005,10 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 // HD Icon
                 case 5:
                     resId = R.drawable.ic_hd_volte;
+                    break;
+                // MemeUi VoLTE
+                case 6:
+                    resId = R.drawable.ic_volte_miui;
                     break;
                 // Dynamic
                 case 1:
