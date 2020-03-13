15c136f81ee495512da944943df54ab60cf5de6c
maxwen <max.weninger@gmail.com>
2020-03-13 22:09:49 +0530

    SystemUI: fix order of init in keyguard indication

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -216,8 +216,6 @@ public class KeyguardIndicationController implements StateListener,
                 Context.DEVICE_POLICY_SERVICE);
         setIndicationArea(indicationArea);
 
-        mBatteryBar = indicationArea.findViewById(R.id.battery_bar_view);
-
         updateDisclosure();
 
         mKeyguardUpdateMonitor.registerCallback(getKeyguardCallback());
@@ -229,6 +227,7 @@ public class KeyguardIndicationController implements StateListener,
     public void setIndicationArea(ViewGroup indicationArea) {
         mIndicationArea = indicationArea;
         mTextView = indicationArea.findViewById(R.id.keyguard_indication_text);
+        mBatteryBar = indicationArea.findViewById(R.id.battery_bar_view);
         mInitialTextColorState = mTextView != null ?
                 mTextView.getTextColors() : ColorStateList.valueOf(Color.WHITE);
         mDisclosure = indicationArea.findViewById(R.id.keyguard_indication_enterprise_disclosure);
