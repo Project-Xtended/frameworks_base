baba4217c495bcb525a197910ad455f3d1ba92b4
rituj <ritujbeniwal@gmail.com>
2020-03-25 16:13:51 +0530

    SystemUI: Keyguard: Make space for fod circle below pattern/pin view

--- packages/SystemUI/src/com/android/keyguard/KeyguardSecurityContainer.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSecurityContainer.java
@@ -23,6 +23,8 @@ import static android.view.WindowInsetsAnimation.Callback.DISPATCH_MODE_STOP;
 
 import static com.android.systemui.DejankUtils.whitelistIpcs;
 
+import static com.android.systemui.util.Utils.getFODHeight;
+
 import static java.lang.Integer.max;
 
 import android.animation.Animator;
@@ -525,10 +527,6 @@ public class KeyguardSecurityContainer extends FrameLayout implements KeyguardSe
 
         // Consume bottom insets because we're setting the padding locally (for IME and navbar.)
         int inset;
-        int minBottomMargin = mHasFod && !mUpdateMonitor.userNeedsStrongAuth() &&
-                mUpdateMonitor.isFingerprintDetectionRunning() ?
-                        getResources().getDimensionPixelSize(
-                                R.dimen.kg_security_container_min_bottom_margin) : 0;
 
         if (sNewInsetsMode == NEW_INSETS_MODE_FULL) {
             int bottomInset = insets.getInsetsIgnoringVisibility(systemBars()).bottom;
@@ -537,8 +535,9 @@ public class KeyguardSecurityContainer extends FrameLayout implements KeyguardSe
         } else {
             inset = insets.getSystemWindowInsetBottom();
         }
-        setPadding(getPaddingLeft(), getPaddingTop(), getPaddingRight(),
-               minBottomMargin > inset ? minBottomMargin : inset);
+        inset = mUpdateMonitor.isUnlockingWithBiometricsPossible(mUpdateMonitor.getCurrentUser()) ?
+                max(getFODHeight(mContext, false), inset) : inset;
+        setPadding(getPaddingLeft(), getPaddingTop(), getPaddingRight(), inset);
         return insets.inset(0, 0, 0, inset);
     }
 
