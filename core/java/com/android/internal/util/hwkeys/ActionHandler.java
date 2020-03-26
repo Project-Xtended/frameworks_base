37935d642e7f7161a5ab792c113402642cd4e519
SuperDroidBond <superdroidbond@yahoo.com>
2020-03-26 13:08:38 +0530

    HwKeys: Enable Partial Screenshot. Change how it works

--- core/java/com/android/internal/util/hwkeys/ActionHandler.java
+++ core/java/com/android/internal/util/hwkeys/ActionHandler.java
@@ -66,6 +66,7 @@ import android.view.IWindowManager;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
+import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
 //import android.view.WindowManagerPolicyControl;
 import android.view.inputmethod.InputMethodManager;
@@ -151,8 +152,7 @@ public class ActionHandler {
         sDisabledActions.add(SYSTEMUI_TASK_ONE_HANDED_MODE_LEFT);
         sDisabledActions.add(SYSTEMUI_TASK_ONE_HANDED_MODE_RIGHT);
         // we need to make this more reliable when the user tap the partial screenshot button
-        // quickly and more times 
-        sDisabledActions.add(SYSTEMUI_TASK_REGION_SCREENSHOT);
+        // quickly and more times
         sDisabledActions.add(SYSTEMUI_TASK_STOP_SCREENPINNING);
         sDisabledActions.add(SYSTEMUI_TASK_ASSISTANT_SOUND_SEARCH);
         sDisabledActions.add(SYSTEMUI_TASK_POWER_MENU);
@@ -499,10 +499,18 @@ public class ActionHandler {
             killProcess(context);
             return;
         } else if (action.equals(SYSTEMUI_TASK_SCREENSHOT)) {
-            sendCommandToWindowManager(new Intent(INTENT_SCREENSHOT));
+            try {
+                WindowManagerGlobal.getWindowManagerService().mokeeTakeScreenshot(WindowManager.TAKE_SCREENSHOT_FULLSCREEN);
+            } catch (RemoteException e) {
+                Log.e(TAG, "Error while trying to take screenshot.", e);
+            }
             return;
         } else if (action.equals(SYSTEMUI_TASK_REGION_SCREENSHOT)) {
-            sendCommandToWindowManager(new Intent(INTENT_REGION_SCREENSHOT));
+            try {
+                WindowManagerGlobal.getWindowManagerService().mokeeTakeScreenshot(WindowManager.TAKE_SCREENSHOT_SELECTED_REGION);
+            } catch (RemoteException e) {
+                Log.e(TAG, "Error while trying to take screenshot.", e);
+            }
             return;
         } else if (action.equals(SYSTEMUI_TASK_SCREENRECORD)) {
             sendCommandToWindowManager(new Intent(INTENT_TOGGLE_SCREENRECORD));
