dbce26a0912210f2a8e2e840c7cea28613583b39
AnkitGourav <Ankitgourav035@gmail.com>
2020-05-23 09:14:37 +0530

    XtendedUtils: add method to get today's passed time
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- core/java/com/android/internal/util/xtended/XtendedUtils.java
+++ core/java/com/android/internal/util/xtended/XtendedUtils.java
@@ -49,6 +49,7 @@ import android.os.Vibrator;
 import android.os.UserHandle;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.text.format.Time;
 import android.view.IWindowManager;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
@@ -470,6 +471,15 @@ public class XtendedUtils {
 
         NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         return wifi.isConnected();
+    }
+
+    // Returns today's passed time in Millisecond
+    public static long getTodayMillis() {
+        final long passedMillis;
+        Time time = new Time();
+        time.set(System.currentTimeMillis());
+        passedMillis = ((time.hour * 60 * 60) + (time.minute * 60) + time.second) * 1000;
+        return passedMillis;
     }
 
     // Check if device is connected to the internet
