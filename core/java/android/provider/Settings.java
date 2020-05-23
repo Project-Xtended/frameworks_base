5621aa9fa2decc9cad3b9a7832bcdd510f3110aa
SuperDroidBond <superdroidbond@yahoo.com>
2020-05-23 20:13:32 +0530

    QsBgNew : Make it toggleable for new gradient QS [1/2]
    Change-Id: I851dd895920fff27b284b82c7273f053f7bc2025

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6821,6 +6821,11 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * @hide
+         */
+        public static final String QS_NEW_BG_ENABLED = "qs_new_bg_enabled";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
