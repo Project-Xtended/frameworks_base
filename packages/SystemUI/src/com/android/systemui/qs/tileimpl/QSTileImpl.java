32d576c7f785679c30b9012b752309741abee912
AnkitGourav <Ankitgourav035@gmail.com>
2020-05-27 14:20:17 +0530

    QSTiles: improve haptic feedback
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -214,9 +214,8 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
             return;
         }
         if (mVibrator != null) {
-            if (mVibrator.hasVibrator()) {
-                mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
-            }
+            if (mVibrator.hasVibrator())
+                mVibrator.vibrate(VibrationEffect.createOneShot(20, VibrationEffect.DEFAULT_AMPLITUDE));
         }
     }
 
