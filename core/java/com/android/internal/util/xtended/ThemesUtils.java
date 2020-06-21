f97f93aa1028dafbe2c7489ea152e320a0eb0919
El Dainosor <eldainosor@gmail.com>
2020-06-21 17:26:57 +0530

    Add InkDrop QS Tile Style [1/3]
    Change-Id: Ic1e08b55ae4826736fef24bcd060586c43d9ed36

--- core/java/com/android/internal/util/xtended/ThemesUtils.java
+++ core/java/com/android/internal/util/xtended/ThemesUtils.java
@@ -96,6 +96,7 @@ public class ThemesUtils {
         "com.android.systemui.qstile.dottedcircle", // 7
         "com.android.systemui.qstile.attemptmountain", // 8
         "com.android.systemui.qstile.squaremedo", // 9
+        "com.android.systemui.qstile.inkdrop", // 10
     };
 
     // Switches qs header style to user selected.
