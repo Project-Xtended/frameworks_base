945a825e569c10ead03184049eb11b597df20a0e
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2020-07-15 14:13:05 +0200

    Add a Tux clock controller
    AshutoshSundresh: Port to Android 11
    
    mClock.refresh(); ------->  mClock.refreshTime();
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AshutoshSundresh <ashutoshsundresh@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -169,6 +169,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new IDEClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new FluidClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new TuxClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
