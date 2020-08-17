f1d1b63863b2422b8fa610f61fc2574af0819fd9
SuperDroidBond <superdroidbond@yahoo.com>
2020-08-18 00:55:20 +0530

    fix mis-merge during cherry-picking
    Change-Id: I09c53f66cb5b31a2e0a06a4dd3d3642a2257f7d3

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -630,34 +630,6 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
         }
     }
 
-    /** @hide */
-    public ExpandableNotificationRow getFirstActiveClearableNotifications(@SelectedRows int selection) {
-        if (mDynamicPrivacyController.isInLockedDownShade()) {
-            return null;
-        }
-        int childCount = getChildCount();
-        for (int i = 0; i < childCount; i++) {
-            View child = getChildAt(i);
-            if (!(child instanceof ExpandableNotificationRow)) {
-                continue;
-            }
-            final ExpandableNotificationRow row = (ExpandableNotificationRow) child;
-            if (matchesSelection(row, selection)) {
-                int visibility = row.getStatusBarNotification().getNotification().visibility;
-                // TODO
-                if (visibility != Notification.VISIBILITY_PUBLIC) {
-                    if (row.getStatusBarNotification().isClearable()) {
-                        return row;
-                    }
-                }
-                if (row.canViewBeDismissed()) {
-                    return row;
-                }
-            }
-        }
-        return null;
-    }
-
     @Override
     @ShadeViewRefactor(RefactorComponent.SHADE_VIEW)
     protected void onFinishInflate() {
@@ -775,8 +747,36 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
         return false;
     }
 
-  @ShadeViewRefactor(RefactorComponent.SHADE_VIEW)
-  public RemoteInputController.Delegate createDelegate() {
+    /** @hide */
+    public ExpandableNotificationRow getFirstActiveClearableNotifications(@SelectedRows int selection) {
+        if (mDynamicPrivacyController.isInLockedDownShade()) {
+            return null;
+        }
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (!(child instanceof ExpandableNotificationRow)) {
+                continue;
+            }
+            final ExpandableNotificationRow row = (ExpandableNotificationRow) child;
+            if (matchesSelection(row, selection)) {
+                int visibility = row.getStatusBarNotification().getNotification().visibility;
+                // TODO
+                if (visibility != Notification.VISIBILITY_PUBLIC) {
+                    if (row.getStatusBarNotification().isClearable()) {
+                        return row;
+                    }
+                }
+                if (row.canViewBeDismissed()) {
+                    return row;
+                }
+            }
+        }
+        return null;
+    }
+
+    @ShadeViewRefactor(RefactorComponent.SHADE_VIEW)
+    public RemoteInputController.Delegate createDelegate() {
         return new RemoteInputController.Delegate() {
             public void setRemoteInputActive(NotificationEntry entry,
                     boolean remoteInputActive) {
