crdroidandroid/android_frameworks_base

MNMLBox Clock: Clean up hacky layouts
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/keyguard/clock/MNMLBoxClockController.java
@@ -76,6 +76,7 @@ public class MNMLBoxClockController implements ClockPlugin {
      */
     private TextView mClock;
     private TextView mDate;
+    private TextView mDateDay;
 
     /**
      * Time and calendars to check the date
@@ -103,7 +104,7 @@ public class MNMLBoxClockController implements ClockPlugin {
                 .inflate(R.layout.digital_mnml_box, null);
         mClock = mView.findViewById(R.id.clock);
         mDate = mView.findViewById(R.id.bigDate);
-        onTimeTick();
+        mDateDay = mView.findViewById(R.id.bigDateDay);
     }
 
     @Override
@@ -111,6 +112,7 @@ public class MNMLBoxClockController implements ClockPlugin {
         mView = null;
         mClock = null;
         mDate = null;
+        mDateDay = null;
     }
 
     @Override
@@ -176,6 +178,8 @@ public class MNMLBoxClockController implements ClockPlugin {
     @Override
     public void setTextColor(int color) {
         mClock.setTextColor(color);
+        mDate.setTextColor(color);
+        mDateDay.setTextColor(color);
     }
 
     @Override
@@ -183,17 +187,6 @@ public class MNMLBoxClockController implements ClockPlugin {
 
     @Override
     public void onTimeTick() {
-        mTime.setTimeInMillis(System.currentTimeMillis());
-        final int hour = mTime.get(Calendar.HOUR) % 12;
-        // lazy and ugly workaround for the it's string
-        String typeHeader = mResources.getQuantityText(
-                R.plurals.type_clock_header, hour).toString();
-        typeHeader = typeHeader.replaceAll("\\n", "");
-        SimpleDateFormat timeformat = new SimpleDateFormat("HH:mm");
-        mClock.setText(typeHeader.substring(0, typeHeader.indexOf("^")) + " " + timeformat.format(mTime.getInstance().getTimeInMillis()));
-        DateFormat dateFormat = DateFormat.getInstanceForSkeleton("EEEEMMMMd", Locale.getDefault());
-        dateFormat.setContext(DisplayContext.CAPITALIZATION_FOR_STANDALONE);
-        mDate.setText(dateFormat.format(mTime.getInstance().getTimeInMillis()));
 
     }
 

Project-Xtended/frameworks_base

Change-Id: I1e85a15d0e03237cd5f013d985d7272ffc09f4de

packages/SystemUI/src/com/android/keyguard/clock/MNMLBoxClockController.java
@@ -76,6 +76,7 @@ public class MNMLBoxClockController implements ClockPlugin {
      */
     private TextView mClock;
     private TextView mDate;
+    private TextView mDateDay;
 
     /**
      * Time and calendars to check the date
@@ -103,7 +104,7 @@ public class MNMLBoxClockController implements ClockPlugin {
                 .inflate(R.layout.digital_mnml_box, null);
         mClock = mBigClockView.findViewById(R.id.clock);
         mDate = mBigClockView.findViewById(R.id.bigDate);
-        onTimeTick();
+        mDateDay = mBigClockView.findViewById(R.id.bigDateDay);
     }
 
     @Override
@@ -111,6 +112,7 @@ public class MNMLBoxClockController implements ClockPlugin {
         mBigClockView = null;
         mClock = null;
         mDate = null;
+        mDateDay = null;
     }
 
     @Override
@@ -176,6 +178,8 @@ public class MNMLBoxClockController implements ClockPlugin {
     @Override
     public void setTextColor(int color) {
         mClock.setTextColor(color);
+        mDate.setTextColor(color);
+        mDateDay.setTextColor(color);
     }
 
     @Override
@@ -188,17 +192,6 @@ public class MNMLBoxClockController implements ClockPlugin {
 
     @Override
     public void onTimeTick() {
-        mTime.setTimeInMillis(System.currentTimeMillis());
-        final int hour = mTime.get(Calendar.HOUR) % 12;
-        // lazy and ugly workaround for the it's string
-        String typeHeader = mResources.getQuantityText(
-                R.plurals.type_clock_header, hour).toString();
-        typeHeader = typeHeader.replaceAll("\\n", "");
-        SimpleDateFormat timeformat = new SimpleDateFormat("HH:mm");
-        mClock.setText(typeHeader.substring(0, typeHeader.indexOf("^")) + " " + timeformat.format(mTime.getInstance().getTimeInMillis()));
-        DateFormat dateFormat = DateFormat.getInstanceForSkeleton("EEEEMMMMd", Locale.getDefault());
-        dateFormat.setContext(DisplayContext.CAPITALIZATION_FOR_STANDALONE);
-        mDate.setText(dateFormat.format(mTime.getInstance().getTimeInMillis()));
     }
 
     @Override