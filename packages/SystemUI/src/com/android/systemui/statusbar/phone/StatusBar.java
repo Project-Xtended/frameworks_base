43e7f51882f269017f5759d16272797c50b9a658
00day0 <therandomuser11@gmail.com>
2020-10-16 10:28:12 +0000

    QS Header data usage info [1/2]

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -173,6 +173,7 @@ import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.qs.QSFragment;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.qs.QuickQSPanel;
+import com.android.systemui.statusbar.info.DataUsageView;
 import com.android.systemui.recents.Recents;
 import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.shared.plugins.PluginManager;
@@ -3914,6 +3915,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateIsKeyguard();
         }
     };
+
+    public void updateQSDataUsageInfo() { 
+        DataUsageView.updateUsage();
+    }
 
     private XSettingsObserver mXSettingsObserver = new XSettingsObserver(mHandler);
     private class XSettingsObserver extends ContentObserver {
