0e4ddbb9a5c24ea78b9876938caebbaf8d31be28
Danny Baumann <dannybaumann@web.de>
2020-11-05 19:13:14 +0000

    Add back increasing ring feature [1/3]
    Change-Id: I2cd34e08badb343fa418ed07e6733e3fbe7d7d5d
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5998,6 +5998,24 @@ public final class Settings {
         public static final String BURN_IN_PROTECTION_INTERVAL = "burn_in_protection_interval";
 
         /**
+         * Whether the phone ringtone should be played in an increasing manner
+         * @hide
+         */
+        public static final String INCREASING_RING = "increasing_ring";
+
+        /**
+         * Start volume fraction for increasing ring volume
+         * @hide
+         */
+        public static final String INCREASING_RING_START_VOLUME = "increasing_ring_start_vol";
+
+        /**
+         * Ramp up time (seconds) for increasing ring
+         * @hide
+         */
+        public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
