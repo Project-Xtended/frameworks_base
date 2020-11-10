aefa2109fb1456bccbfebb830a2230caf2520d15
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-10 18:45:10 +0200

    base: Only allow edge lights on AOD when AOD is enabled [1/2]
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayoutController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayoutController.java
@@ -1248,7 +1248,7 @@ public class NotificationStackScrollLayoutController {
                 continue;
             }
             final ExpandableNotificationRow row = (ExpandableNotificationRow) child;
-            if (row.canViewBeDismissed() && NotificationStackScrollLayout.matchesSelection(row, selection)) {
+            if (row.getEntry().isClearable() && NotificationStackScrollLayout.matchesSelection(row, selection)) {
                 return row;
             }
         }
