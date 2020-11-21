d7998eba578acff0cd0f8166418ad0772c600f15
ezio84 <brabus84@gmail.com>
2020-11-21 19:54:09 +0100

    Allow back swipe on bottom screen if launcher is showing
    on the very bottom area of the screen (1/4 of display size)
    so we can trigger our actions everywhere
    
    Change-Id: I0ca2f5981664864d19a2560a77cfea6e95d304ae
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -609,7 +609,7 @@ public class NavigationBarView extends FrameLayout implements TunerService.Tunab
         if (!visible) {
             mTransitionListener.onBackAltCleared();
         }
-        mImeVisible = visible;
+        mEdgeBackGestureHandler.setImeVisible(visible);
         mRotationButtonController.getRotationButton().setCanShowRotationButton(!visible);
     }
 
