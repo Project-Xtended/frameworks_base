2f25186b2ffcf80264fe8ca71940def03fb087e4
DennySPb <dennyspb@gmail.com>
2020-11-24 12:45:30 +0300

    LongSwipe: change helper for screenshot action
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I35090be013d76d6d5eb740ee48bd543e2bbf1cbe

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -779,7 +779,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
                 ActionUtils.switchScreenOff(mContext);
                 break;
             case 8: // Screenshot
-                ActionUtils.takeScreenshot(true);
+                XtendedUtils.takeScreenshot(true);
                 break;
             case 9: // Notification panel
                 ActionUtils.toggleNotifications();
