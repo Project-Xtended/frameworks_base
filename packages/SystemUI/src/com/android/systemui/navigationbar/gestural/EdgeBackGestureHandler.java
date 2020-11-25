87b559899f151443a6966eaecb594e6a25bcbf74
ezio84 <brabus84@gmail.com>
2020-11-25 19:59:56 +0100

    Advanced nav gestures: improve code and fix glitches
    fix  arrows animation glitch after triggering an action
    with the extended swipe
    
    hide the arrows before triggering a screenshot action
    
    code cleanup
    
    Change-Id: I5f01938b6b688acd14225f58ce5a2032f083f92f
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -251,7 +251,6 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
     private boolean mIsExtendedSwipe;
     private int mLeftVerticalSwipeAction;
     private int mRightVerticalSwipeAction;
-    private boolean mBlockNextEvent;
     private Handler mHandler;
     private boolean mImeVisible;
 
@@ -947,7 +946,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                     mIsBackGestureAllowed,
                     QuickStepContract.isBackGestureDisabled(mSysUiFlags), mDisplaySize,
                     mEdgeWidthLeft, mLeftInset, mEdgeWidthRight, mRightInset, mExcludeRegion));
-        } else if ((mAllowGesture || mLogGesture) && !mBlockNextEvent) {
+        } else if (mAllowGesture || mLogGesture) {
             if (!mThresholdCrossed) {
                 // mThresholdCrossed is true only after the first move event
                 // then other events will go straight to "forward touch" line
@@ -1037,9 +1036,6 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 mEdgeBackPlugin.onMotionEvent(ev);
                 dispatchToBackAnimation(ev);
             }
-        } else if (mBlockNextEvent) {
-            mBlockNextEvent = false;
-            cancelGesture(ev);
         }
 
         mProtoTracer.scheduleFrameUpdate();
@@ -1060,8 +1056,11 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
     }
 
     private void prepareForAction() {
-        mBlockNextEvent = true;
-        mEdgeBackPlugin.resetOnDown();
+        // cancel touch event then trigger the action
+        final long now = SystemClock.uptimeMillis();
+        final MotionEvent ev = MotionEvent.obtain(now, now,
+                MotionEvent.ACTION_CANCEL, 0.0f, 0.0f, 0);
+        cancelGesture(ev);
     }
 
     private void triggerAction(boolean isVertical) {
