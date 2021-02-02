462f4c186628a30af9fd852e94b6e7a46c0b5d4a
Advaith Bhat <advaithbhat9@gmail.com>
2021-02-02 19:43:32 +0530

    base: Fixup On-The-Go Toggle in Powermenu

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -1554,7 +1554,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         @Override
         public void onPress() {
             ComponentName cn = new ComponentName("com.android.systemui",
-                    "com.android.systemui.benzo.onthego.OnTheGoService");
+                    "com.android.systemui.xtended.onthego.OnTheGoService");
             Intent onTheGoIntent = new Intent();
             onTheGoIntent.setComponent(cn);
             onTheGoIntent.setAction("start");
