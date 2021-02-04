e206efd2982e493f3e96c787db1c21b6f9253bc4
Laker <lakerman1987@gmail.com>
2021-02-04 18:45:45 +0300

    SystemUI: add more FOD icons [1/2]

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -125,6 +125,8 @@ public class FODCircleView extends ImageView {
         R.drawable.fod_icon_scratch_pink_blue,
         R.drawable.fod_icon_scratch_red_blue,
         R.drawable.fod_icon_fire_ice_ouroboros,
+        R.drawable.fod_icon_light,
+        R.drawable.fod_icon_gxzw,
         R.drawable.fod_icon_transparent
     };
 
