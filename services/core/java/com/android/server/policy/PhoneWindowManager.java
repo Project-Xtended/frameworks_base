0401e9e7bef148801fa7a50e7d28f177fdc8ec3f
Pranav Vashi <neobuddy89@gmail.com>
2021-03-14 09:37:18 +0530

    Fix screenshot type for three-finger-screenshot
    * If we don't mention it, it will use screenshot type used previously.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1980,7 +1980,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mSwipeToScreenshot = new SwipeToScreenshotListener(context, new SwipeToScreenshotListener.Callbacks() {
             @Override
             public void onSwipeThreeFinger() {
-                mHandler.post(mScreenshotRunnable);
+                    takeScreenshot(TAKE_SCREENSHOT_FULLSCREEN, 0, SCREENSHOT_KEY_OTHER);
             }
         });
         mWakeGestureListener = new MyWakeGestureListener(mContext, mHandler);
