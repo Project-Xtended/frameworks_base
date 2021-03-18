d38450d0069297bb958bdfc21bb0b44532060c29
Danny Lin <danny@kdrag0n.dev>
2021-03-18 19:13:20 -0300

    SystemUI: Always refresh power menu on UI mode change
      This is necessary for reliably theming the global actions dialog with
      dynamic colors and adapting it to light/dark themes.
    
      Demo screenshots (with color overlays applied):
      https://twitter.com/kdrag0n/status/1445960685427433473
    
    Change-Id: Ib1b92c5f2fc5864dbd1bcd9e4e224b2768f73623

---
+++ commits.sh
@@ -0,0 +1,4 @@
+git fetch "https://review.arrowos.net/ArrowOS/android_frameworks_base" refs/changes/50/15050/1 && git cherry-pick FETCH_HEAD
+git fetch "https://review.arrowos.net/ArrowOS/android_frameworks_base" refs/changes/51/15051/1 && git cherry-pick FETCH_HEAD
+git fetch "https://review.arrowos.net/ArrowOS/android_frameworks_base" refs/changes/52/15052/1 && git cherry-pick FETCH_HEAD
+git fetch "https://review.arrowos.net/ArrowOS/android_frameworks_base" refs/changes/49/15049/1 && git cherry-pick FETCH_HEAD
