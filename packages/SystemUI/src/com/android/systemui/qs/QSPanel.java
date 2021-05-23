51d0b34b4df2bf1d49b9451d6e6917178e2709e2
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-05-23 14:22:04 +0200

    SystemUI: QS: minimize detail view to middle of the button
    Test: visual observation
    Signed-off-by: Timi Rautamäki <timi.rautamaki@gmail.com>
    Change-Id: I46b9be454ee5a699833ce0d8608df2e545e0f905

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -1000,7 +1000,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         }
         r.tile.setDetailListening(show);
         int x = r.tileView.getLeft() + r.tileView.getWidth() / 2;
-        int y = r.tileView.getDetailY() + mTileLayout.getOffsetTop(r) + getTop();
+        int y = r.tileView.getDetailY() + getTop();
         handleShowDetailImpl(r, show, x, y);
     }
 
