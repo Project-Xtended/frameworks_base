f4696ec24900c2fede5d53a6c27310a0026100a5
mukesh22584 <mks22584@gmail.com>
2021-05-24 13:39:01 +0530

    A little slower animation duration

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -948,9 +948,9 @@ public class WindowManagerService extends IWindowManager.Stub
     PowerManager mPowerManager;
     PowerManagerInternal mPowerManagerInternal;
 
-    private float mWindowAnimationScaleSetting = 1.0f;
-    private float mTransitionAnimationScaleSetting = 1.0f;
-    private float mAnimatorDurationScaleSetting = 1.0f;
+    private float mWindowAnimationScaleSetting = 0.51f;
+    private float mTransitionAnimationScaleSetting = 0.51f;
+    private float mAnimatorDurationScaleSetting = 0.51f;
     private boolean mAnimationsDisabled = false;
     boolean mPointerLocationEnabled = false;
     private boolean mAnimationsForceDisabled = false;
