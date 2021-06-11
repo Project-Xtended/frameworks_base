e5d49113a7275c0e290558f5c5ae8a8f106ad5fb
SuperDroidBond <superdroidbond@yahoo.com>
2021-06-11 22:52:22 +0530

    base: Xtended System-wide Slider Styles [1/3]

--- core/java/com/android/internal/util/xtended/ThemesUtils.java
+++ core/java/com/android/internal/util/xtended/ThemesUtils.java
@@ -105,6 +105,23 @@ public class ThemesUtils {
             "com.android.systemui.brightness.slider.memestroke",
     };
 
+    // System-Wide Slider Styles, DU-Way
+    public static final String[] SYSTEM_SLIDER_DANIEL = {
+            "com.android.system.slider.daniel",
+    };
+    public static final String[] SYSTEM_SLIDER_MEMEMINII = {
+            "com.android.system.slider.mememini",
+    };
+    public static final String[] SYSTEM_SLIDER_MEMEROUND = {
+            "com.android.system.slider.memeround",
+    };
+    public static final String[] SYSTEM_SLIDER_MEMEROUNDSTROKE = {
+            "com.android.system.slider.memeroundstroke",
+    };
+    public static final String[] SYSTEM_SLIDER_MEMESTROKE = {
+            "com.android.system.slider.memestroke",
+    };
+
     // Switches qs tile style to user selected.
     public static void updateNewTileStyle(IOverlayManager om, int userId, int qsTileStyle) {
         if (qsTileStyle == 0) {
