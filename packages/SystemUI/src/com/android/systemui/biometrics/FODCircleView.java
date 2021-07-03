646159ffcdd18d61a662d871c8625b1f14a91e31
Pranav Vashi <neobuddy89@gmail.com>
2021-07-03 17:08:12 +0530

    FODCircleView: Hide if fingerprint detection stops
    * Otherwise we see FOD circle hanging around till screen is turn off.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -169,6 +169,8 @@ public class FODCircleView extends ImageView {
                 BiometricSourceType biometricSourceType) {
             if (biometricSourceType == BiometricSourceType.FINGERPRINT) {
                 mIsBiometricRunning = running;
+                if (!mIsBiometricRunning)
+                    hide();
             }
         }
 
