8c881fc2987f8cfb93520c7405731c27eed4e74c
SuperDroidBond <superdroidbond@yahoo.com>
2021-07-12 22:44:51 +0530

    SystemUI: LS Hide Shortcut must hide all shortcuts not just defaults
    SDB: after https://github.com/Project-Xtended/frameworks_base/commit/97ba826ec3d4d920437d9465d6d589e588d5355a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -428,7 +428,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
     private void updateRightAffordanceIcon() {
         IconState state = mRightButton.getIcon();
-        mRightAffordanceView.setVisibility(!mDozing && state.isVisible ? View.VISIBLE : View.GONE);
+        mRightAffordanceView.setVisibility(!hideShortcuts() && !mDozing && state.isVisible ? View.VISIBLE : View.GONE);
         if (state.isVisible) {
             if (state.drawable != mRightAffordanceView.getDrawable()
                     || state.tint != mRightAffordanceView.shouldTint()
@@ -510,7 +510,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     }
 
     private void updateLeftAffordanceIcon() {
-        if (!mShowLeftAffordance || mDozing) {
+        if (!mShowLeftAffordance || mDozing || hideShortcuts()) {
             mLeftAffordanceView.setVisibility(GONE);
             return;
         }
