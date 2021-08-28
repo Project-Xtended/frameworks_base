9093eb73653be085dd6fe27dbe0621ef05427059
SagarMakhar <sagarmakhar@gmail.com>
2021-08-28 14:15:37 +0000

    Allow locking tasks to recents [1/2]
    Change-Id: Id18af94cf52db46054299a862b91c739e8d204f6
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5971,6 +5971,11 @@ public final class Settings {
         public static final String PULSE_LIGHT_LAYOUT_STYLE = "pulse_light_layout_style";
 
         /**
+         * @hide
+         */
+        public static final String RECENTS_LOCKED_TASKS = "recents_locked_tasks";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
