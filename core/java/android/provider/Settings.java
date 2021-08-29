fc6a0622fd8f230e0d20bd524a72323ddb0a3a26
SuperDroidBond <superdroidbond@yahoo.com>
2021-08-29 23:16:28 +0530

    User Selectable VoLTE Icons [1/2]
    SDB: Nothing new, just adapted for Dynamic Icon set for icons

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6936,6 +6936,12 @@ public final class Settings {
         public static final String AUTO_BRIGHTNESS_ONE_SHOT = "auto_brightness_one_shot";
 
         /**
+         * VoLTE icon style
+         * @hide
+         */
+        public static final String VOLTE_ICON_STYLE = "volte_icon_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
