772824eda0507eee4f449f31552f5e84132493a4
jhonboy121 <alfredmathew05@gmail.com>
2021-11-15 01:15:37 +0530

    base: allow toggling location privacy indicator[1/2]
    SDB: also remove the old location indicator tuner switch
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11004,6 +11004,13 @@ public final class Settings {
         public static final String GAME_DASHBOARD_ALWAYS_ON = "game_dashboard_always_on";
 
         /**
+         * Whether to show privacy indicator for location
+         * Default value is based on it's device config flag
+         * @hide
+         */
+        public static final String ENABLE_LOCATION_PRIVACY_INDICATOR = "enable_location_privacy_indicator";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
