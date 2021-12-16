034ed7681b5950d2b16c018344e1683f48426b44
HearnYuyuko <dingwenxuan4@gmail.com>
2021-12-16 15:55:41 +0000

    base: Fix ScreenRecord for GamingMode Android S SysUI's ScreenRecord is similar than a11's ,so i change this startRecording() like a11
    Change-Id: I3dec02689d50ecc09aca878d28d8c3e0d2756de8

--- packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
@@ -160,17 +160,6 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
                                  intent.getBooleanExtra(EXTRA_SHOW_STOP_DOT, false), intent.getBooleanExtra(EXTRA_LOW_QUALITY, false),
                                  intent.getBooleanExtra(EXTRA_LONGER_DURATION, false));
 
-                if (startRecording()) {
-                    updateState(true);
-                    createRecordingNotification();
-                    mUiEventLogger.log(Events.ScreenRecordEvent.SCREEN_RECORD_START);
-                } else {
-                    updateState(false);
-                    createErrorNotification();
-                    stopForeground(true);
-                    stopSelf();
-                    return Service.START_NOT_STICKY;
-                }
                 break;
 
             case ACTION_STOP_NOTIF:
@@ -233,7 +222,7 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
 
     protected void doStartRecording(int audioSource, boolean showTaps, boolean showStopDot,
                                     boolean lowQuality, boolean longerDuration) {
-        int mCurrentUserId = mUserContextTracker.getUserContext().getUserId();
+        int currentUserId = mUserContextTracker.getUserContext().getUserId();
         mAudioSource = ScreenRecordingAudioSource
                 .values()[audioSource];
         Log.d(TAG, "recording with audio source" + mAudioSource);
@@ -251,7 +240,7 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
 
         mRecorder = new ScreenMediaRecorder(
                 mUserContextTracker.getUserContext(),
-                mCurrentUserId,
+                currentUserId,
                 mAudioSource,
                 this
         );
@@ -299,15 +288,20 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
      * Begin the recording session
      * @return true if successful, false if something went wrong
      */
-    private boolean startRecording() {
+    private void startRecording() {
         try {
             getRecorder().start();
-            return true;
+            updateState(true);
+            createRecordingNotification();
+            mUiEventLogger.log(Events.ScreenRecordEvent.SCREEN_RECORD_START);
         } catch (IOException | RemoteException | RuntimeException e) {
             showErrorToast(R.string.screenrecord_start_error);
             e.printStackTrace();
+            updateState(false);
+            createErrorNotification();
+            stopForeground(true);
+            stopSelf();
         }
-        return false;
     }
 
     /**
