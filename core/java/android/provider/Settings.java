e19e9f43f94cbc0a881a6b5ef3386743688903df
dlwlrma123 <alexfinhart@gmail.com>
2021-12-21 06:50:34 +0800

    GamingMode: make gaming_mode_enable @readable * for some reasons this was causing apps force closing on some devices.
    12-19 20:13:23.073  7564  7564 E AndroidRuntime: FATAL EXCEPTION: main
    12-19 20:13:23.073  7564  7564 E AndroidRuntime: Process: com.google.android.inputmethod.latin, PID: 7564
    12-19 20:13:23.073  7564  7564 E AndroidRuntime: java.lang.SecurityException: Settings key: <gaming_mode_enabled> is not readable. From S+, settings keys annotated with @hide are restricted to system_server and system apps only, unless they are annotated with @Readable
    
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6160,6 +6160,7 @@ public final class Settings {
           * Setting to enable/disable gaming mode
           * @hide
           */
+        @Readable
         public static final String GAMING_MODE_ENABLED = "gaming_mode_enabled";
 
         /**
