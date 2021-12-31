828a7e7b69481c6f9be0ccc85c5fb555c8fc1a6d
SagarMakhar <sagarmakhar@gmail.com>
2021-12-31 23:53:48 +0530

    SystemUI: Apply monet to fingerprint authentication ripple animation
    Change-Id: I15d0f9fee13647062c651219b527e01a539b6909

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleView.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleView.kt
@@ -28,6 +28,7 @@ import android.view.View
 import android.provider.Settings
 import android.view.animation.PathInterpolator
 import com.android.internal.graphics.ColorUtils
+import com.android.settingslib.Utils
 import com.android.systemui.animation.Interpolators
 import com.android.systemui.statusbar.charging.RippleShader
 
@@ -73,7 +74,7 @@ class AuthRippleView(context: Context?, attrs: AttributeSet?) : View(context, at
         }
 
     init {
-        rippleShader.color = 0xffffffff.toInt() // default color
+        rippleShader.color = Utils.getColorAttr(context, android.R.attr.colorAccent).defaultColor
         rippleShader.progress = 0f
         rippleShader.sparkleStrength = RIPPLE_SPARKLE_STRENGTH
         ripplePaint.shader = rippleShader
