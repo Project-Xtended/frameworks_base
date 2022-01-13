b12dde8170c028f44bd902b314578bf6f6670c6a
jhenrique09 <jhsv09@gmail.com>
2022-01-13 13:33:11 -0300

    SystemUIGoogle: Import Reverse Charging and Battery Saver tiles
    [spezi77 BatterySaverTileGoogle edits] Added keyguardStateController to constructor list
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I6f25f808910bbeb7d06b3602d8227bb132625c8d

--- packages/SystemUI/src/org/pixelexperience/systemui/qs/tileimpl/QSFactoryImplGoogle.java
+++ packages/SystemUI/src/org/pixelexperience/systemui/qs/tileimpl/QSFactoryImplGoogle.java
@@ -60,9 +60,9 @@ import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.VpnTile;
-import com.google.android.systemui.qs.tiles.BatterySaverTileGoogle;
-import com.google.android.systemui.qs.tiles.OverlayToggleTile;
-import com.google.android.systemui.qs.tiles.ReverseChargingTile;
+
+import org.pixelexperience.systemui.qs.tiles.BatterySaverTileGoogle;
+import org.pixelexperience.systemui.qs.tiles.ReverseChargingTile;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -72,7 +72,6 @@ import dagger.Lazy;
 @SysUISingleton
 public class QSFactoryImplGoogle extends QSFactoryImpl {
     private final Provider<BatterySaverTileGoogle> mBatterySaverTileGoogleProvider;
-    private final Provider<OverlayToggleTile> mOverlayToggleTileProvider;
     private final Provider<ReverseChargingTile> mReverseChargingTileProvider;
 
     @Inject
@@ -118,7 +117,6 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
             Provider<RefreshRateTile> refreshRateTileProvider,
             Provider<OnTheGoTile> onTheGoTileProvider,
             Provider<FPSInfoTile> fpsInfoTileProvider,
-            Provider<OverlayToggleTile> overlayToggleTileProvider,
             Provider<ReverseChargingTile> reverseChargingTileProvider) {
         super(qsHostLazy,
                 customTileBuilderProvider,
@@ -163,7 +161,6 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
                 fpsInfoTileProvider);
         this.mReverseChargingTileProvider = reverseChargingTileProvider;
         this.mBatterySaverTileGoogleProvider = batterySaverTileGoogleProvider;
-        this.mOverlayToggleTileProvider = overlayToggleTileProvider;
     }
 
     @Override
@@ -176,10 +173,10 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
     }
 
     private QSTileImpl createTileInternal(String str) {
-        if (str.equals("ott")) {
-            return this.mOverlayToggleTileProvider.get();
-        } else if (str.equals("reverse")) {
+        if (str.equals("reverse")) {
             return this.mReverseChargingTileProvider.get();
+        } else if (str.equals("battery")) {
+            return this.mBatterySaverTileGoogleProvider.get();
         }
         return null;
     }
