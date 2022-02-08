ce9f28018fa58b65477f0e33b97139fbfbb2d501
LuK1337 <priv.luk@gmail.com>
2022-02-09 06:50:27 +0800

    SystemUI: Make center clock layout safe insets aware This effectively fixes wrongly centered clock layout on devices with side notch / hole punch.
    Change-Id: I7cb8cbbe628397c50d37e23ba1853368e61a1fc9
    
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -41,6 +41,7 @@ import android.view.DisplayCutout;
 import android.view.Gravity;
 import android.view.IWindowManager;
 import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
@@ -509,6 +510,15 @@ public class PhoneStatusBarView extends PanelBar implements Callbacks, TunerServ
                 getPaddingTop(),
                 size.x - contentRect.right,
                 getPaddingBottom());
+
+        // Apply negative paddings to centered area layout so that we'll actually be on the center.
+        final int winRotation = getDisplay().getRotation();
+        LayoutParams centeredAreaParams =
+                (LayoutParams) findViewById(R.id.center_clock_layout).getLayoutParams();
+        centeredAreaParams.leftMargin =
+                winRotation == Surface.ROTATION_0 ? -contentRect.left : 0;
+        centeredAreaParams.rightMargin =
+                winRotation == Surface.ROTATION_0 ? -(size.x - contentRect.right) : 0;
     }
 
     public void setHeadsUpVisible(boolean headsUpVisible) {
