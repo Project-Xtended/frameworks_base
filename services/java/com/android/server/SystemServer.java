962423531c1c9e792488a0e5337a5259adeae6ec
LibXZR <i@xzr.moe>
2022-02-09 09:58:48 +0800

    Add support for GMS switch
    @neobuddy89:
    * Adapted for A13 QPR1
    * Enabled by default
    * Updated package list
    * Inject same package list in ParallelSpaceManagerService
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -217,6 +217,7 @@ import com.android.server.wm.WindowManagerService;
 
 import dalvik.system.VMRuntime;
 
+import ink.kaleidoscope.server.GmsManagerService;
 import ink.kaleidoscope.server.ParallelSpaceManagerService;
 
 import java.io.File;
@@ -408,6 +409,8 @@ public final class SystemServer implements Dumpable {
             "com.android.server.media.MediaCommunicationService";
     private static final String APP_COMPAT_OVERRIDES_SERVICE_CLASS =
             "com.android.server.compat.overrides.AppCompatOverridesService$Lifecycle";
+    private static final String GMS_MANAGER_SERVICE_SERVICE_CLASS =
+            "ink.kaleidoscope.server.GmsManagerService";
     private static final String PARALLEL_SPACE_SERVICE_CLASS =
             "ink.kaleidoscope.server.ParallelSpaceManagerService";
 
@@ -2788,6 +2791,10 @@ public final class SystemServer implements Dumpable {
 
         t.traceBegin("AppCompatOverridesService");
         mSystemServiceManager.startService(APP_COMPAT_OVERRIDES_SERVICE_CLASS);
+        t.traceEnd();
+
+        t.traceBegin("StartGmsManagerService");
+        mSystemServiceManager.startService(GMS_MANAGER_SERVICE_SERVICE_CLASS);
         t.traceEnd();
 
         t.traceBegin("StartParallelSpaceManagerService");
