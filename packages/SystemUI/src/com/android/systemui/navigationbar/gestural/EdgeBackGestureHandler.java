984ee2df262507435a87f40255b15933a5db4a95
DennySPb <dennyspb@gmail.com>
2022-02-18 11:14:12 +0300

    EdgeBackGesture: Remove assistant action
        * Have NPE with columbus
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Id29597df37d75d830d7b99d2cacb817830ba9e77
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -69,7 +69,6 @@ import com.android.internal.util.LatencyTracker;
 import com.android.internal.util.xtended.XtendedUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
-import com.android.systemui.assist.AssistManager;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
@@ -244,7 +243,6 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
     private boolean mInRejectedExclusion = false;
     private boolean mIsOnLeftEdge;
 
-    private AssistManager mAssistManager;
     private int mTimeout = 2000; //ms
     private int mLeftLongSwipeAction;
     private int mRightLongSwipeAction;
@@ -419,7 +417,6 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 mContext.getMainThreadHandler(), mContext, this::onNavigationSettingsChanged);
 
         mHandler = new Handler();
-        mAssistManager = Dependency.get(AssistManager.class);
         updateCurrentUserResources();
     }
 
@@ -1075,8 +1072,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
             case 0: // No action
             default:
                 break;
-            case 1: // Assistant
-                mAssistManager.startAssist(new Bundle() /* args */);
+            case 1:
                 break;
             case 2: // Voice search
                 ActionUtils.launchVoiceSearch(mContext);
