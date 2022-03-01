a4e6cb3a1c87fedf1b79e0d19d8b617bc73ce7d6
cjh1249131356 <cjh1249131356@gmail.com>
2022-03-01 18:06:59 +0800

    base: Allow to skip confirmation in biometric auth dialog [1/2]
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Change-Id: I23f414b341a309b32b378503fe58d7ba11b0e6f4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12207,6 +12207,13 @@ public final class Settings {
         public static final String TAP_GESTURE = "tap_gesture";
 
         /**
+         * Whether to skip biometric auth confirmation
+         * @hide
+         */
+        @Readable
+        public static final String IGNORE_AUTH_CONFIRMATION = "ignore_auth_confirmation";
+
+        /**
          * Controls whether the people strip is enabled.
          * @hide
          */
