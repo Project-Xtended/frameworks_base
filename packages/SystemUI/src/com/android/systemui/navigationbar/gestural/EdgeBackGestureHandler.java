pixeldust-project-caf/android_frameworks_base

Gestures nav: Add Skip/Previous Song Function abcdu: use our statusbar media actions implementation to have also remote spotify support, and to be able to send different media actions from any class with the proper keycode
Signed-off-by: spezi77 <spezi7713@gmx.net>
Change-Id: Ie7cd23c368a02a530cd1fa71076251bb3918f3cb

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1029,6 +1029,12 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
             case 13: // Kill app
                 PixeldustUtils.killForegroundApp();
                 break;
+            case 14: // Skip song
+                PixeldustUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_NEXT);
+                break;
+            case 15: // Previous song
+                PixeldustUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
+                break;
         }
     }
 

Project-Xtended/frameworks_base

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1013,6 +1013,12 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
             case 13: // Kill app
                 XtendedUtils.killForegroundApp();
                 break;
+            case 14: // Skip song
+                XtendedUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_NEXT);
+                break;
+            case 15: // Previous song
+                XtendedUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
+                break;
         }
     }
 