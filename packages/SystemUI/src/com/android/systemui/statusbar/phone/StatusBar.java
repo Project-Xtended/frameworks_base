AospExtended/platform_frameworks_base

SystemUI: Fix heads up blacklists/stoplists initial on boot
* Initial should be after presenter set up otherwise it will be null.
  Also improve some codes.

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
Change-Id: I9be7cf2b1b1f7f5e443240a4cfee9c00312c9e2e

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1074,11 +1074,11 @@ public class StatusBar extends SystemUI implements DemoMode,
             Log.v(TAG, "start(): no wallpaper service ");
         }
 
-        mSbSettingsObserver.observe();
-        mSbSettingsObserver.update();
-
         // Set up the initial notification state. This needs to happen before CommandQueue.disable()
         setUpPresenter();
+
+        mSbSettingsObserver.observe();
+        mSbSettingsObserver.update();
 
         if (containsType(result.mTransientBarTypes, ITYPE_STATUS_BAR)) {
             showTransientUnchecked();
@@ -4950,8 +4950,13 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.FORCE_SHOW_NAVBAR))) {
                 updateNavigationBarVisibility();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_STOPLIST_VALUES))) {
+                setHeadsUpStoplist();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_BLACKLIST_VALUES))) {
+                setHeadsUpBlacklist();
             }
-            update();
         }
 
         public void update() {

Project-Xtended/frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1061,11 +1061,11 @@ public class StatusBar extends SystemUI implements DemoMode,
             Log.v(TAG, "start(): no wallpaper service ");
         }
 
-        mXtndSettingsObserver.observe();
-        mXtndSettingsObserver.update();
-
         // Set up the initial notification state. This needs to happen before CommandQueue.disable()
         setUpPresenter();
+
+        mXtndSettingsObserver.observe();
+        mXtndSettingsObserver.update();
 
         if (containsType(result.mTransientBarTypes, ITYPE_STATUS_BAR)) {
             showTransientUnchecked();
@@ -4964,8 +4964,13 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.Secure.getUriFor(
                     Settings.Secure.PULSE_ON_NEW_TRACKS))) {
                 setPulseOnNewTracks();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_STOPLIST_VALUES))) {
+                setHeadsUpStoplist();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_BLACKLIST_VALUES))) {
+                setHeadsUpBlacklist();
             }
-            update();
         }
 
         public void update() {