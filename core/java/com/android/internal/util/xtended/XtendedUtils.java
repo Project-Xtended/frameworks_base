244a11db7b6629166ee1ce9afd234d8d957f2170
maxwen <max.weninger@gmail.com>
2022-04-27 17:33:18 +0800

    SystemUI: QS vertical layout & hide label settings [1/2]
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- core/java/com/android/internal/util/xtended/XtendedUtils.java
+++ core/java/com/android/internal/util/xtended/XtendedUtils.java
@@ -59,6 +59,7 @@ import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.Vibrator;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
@@ -337,6 +338,21 @@ public class XtendedUtils {
             case AudioManager.RINGER_MODE_SILENT:
                 am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
                 break;
+        }
+    }
+    
+    public static class QSLayoutUtils {
+
+        public static boolean getQSTileLabelHide(Context context) {
+            return Settings.System.getIntForUser(context.getContentResolver(),
+                    Settings.System.QS_TILE_LABEL_HIDE,
+                    0, UserHandle.USER_CURRENT) == 1;
+        }
+
+        public static boolean getQSTileVerticalLayout(Context context) {
+            return Settings.System.getIntForUser(context.getContentResolver(),
+                    Settings.System.QS_TILE_VERTICAL_LAYOUT,
+                    0, UserHandle.USER_CURRENT) == 1;
         }
     }
 }
