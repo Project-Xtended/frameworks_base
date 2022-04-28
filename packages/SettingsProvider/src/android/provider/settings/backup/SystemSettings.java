dba6977fbf2e15d3d31b9d300c5e599e3f782381
cjh1249131356 <cjh1249131356@gmail.com>
2022-04-28 16:09:16 +0800

    base: QS tiles layout customizations [1/2]
    - Inspired from https://github.com/omnirom/android_frameworks_base/commit/4d71992f4446d828e0f5e7eb3841632ea28ee71c.
      To avoid breaking aosp performance of calculating stuff here and there, use overlay to implement this.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Cruzer1 <preetamswain143@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -98,5 +98,7 @@ public class SystemSettings {
         Settings.System.DOZE_ON_CHARGE,
 	    Settings.System.QS_TILE_LABEL_HIDE,
         Settings.System.QS_TILE_VERTICAL_LAYOUT,
+        Settings.System.QS_LAYOUT,
+        Settings.System.QQS_LAYOUT,
     };
 }
