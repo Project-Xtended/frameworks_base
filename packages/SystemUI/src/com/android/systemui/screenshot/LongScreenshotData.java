51301087267f9b91e4e1e159e21d78aef5457c78
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-26 01:39:00 +0300

    SystemUI: screenshot: disable magnification for partial screenshots
    Partial screenshots are screen-size and don't need a magnifying glass.
    
    Change-Id: I472103352411d1482423230a4ff08ddda45cc53d
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/LongScreenshotData.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/LongScreenshotData.java
@@ -32,6 +32,8 @@ public class LongScreenshotData {
     private final AtomicReference<ScreenshotController.TransitionDestination>
             mTransitionDestinationCallback;
 
+    private boolean mNeedsMagnification;
+
     @Inject
     public LongScreenshotData() {
         mLongScreenshot = new AtomicReference<>();
@@ -72,5 +74,13 @@ public class LongScreenshotData {
      */
     public ScreenshotController.TransitionDestination takeTransitionDestinationCallback() {
         return mTransitionDestinationCallback.getAndSet(null);
+    }
+
+    public void setNeedsMagnification(boolean status) {
+        mNeedsMagnification = status;
+    }
+
+    public boolean getNeedsMagnification() {
+        return mNeedsMagnification;
     }
 }
--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -721,6 +721,7 @@ public class ScreenshotController {
         ScrollCaptureController.BitmapScreenshot bitmapScreenshot =
             new ScrollCaptureController.BitmapScreenshot(mContext, newScreenshot);
 
+        mLongScreenshotHolder.setNeedsMagnification(false);
         startLongScreenshotActivity(bitmapScreenshot);
     }
 
@@ -789,6 +790,7 @@ public class ScreenshotController {
                 return;
             }
 
+            mLongScreenshotHolder.setNeedsMagnification(true);
             startLongScreenshotActivity(longScreenshot);
         }, mMainExecutor);
     }
