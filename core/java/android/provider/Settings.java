8834c4ee7b3b3cb40ea0ddbc25c0a8a304d1d172
Pranav Vashi <neobuddy89@gmail.com>
2022-09-09 18:58:24 +0530

    Allow setting custom symbol near text on charging [1/2]
    [SuperDroidBond: Thanks to @neobuddy89. I am just bringing it to Q n R n S n T]
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5950,6 +5950,12 @@ public final class Settings {
         public static final String QS_SHOW_BATTERY_ESTIMATE = "qs_show_battery_estimate";
 
         /**
+         * Charging symbol near battery text percentage
+         * @hide
+         */
+        public static final String TEXT_CHARGING_SYMBOL = "text_charging_symbol";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
