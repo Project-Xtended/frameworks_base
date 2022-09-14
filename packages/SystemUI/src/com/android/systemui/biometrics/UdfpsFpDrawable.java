43eab23529b29c88cc1be8036e5f7b8c330e67a9
SuperDroidBond <superdroidbond@yahoo.com>
2022-09-14 15:26:34 +0530

    UDFPS: bring back UdfpsDrawable & UdfpsFpDrwable java files
    This commit partially reverts 05c83b715a463388d45f2c0aaff5b4225541e1c4
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsFpDrawable.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsFpDrawable.java
@@ -13,19 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.systemui.biometrics
 
-import android.content.Context
-import android.graphics.Canvas
+package com.android.systemui.biometrics;
+
+import android.content.Context;
+import android.graphics.Canvas;
+
+import androidx.annotation.NonNull;
 
 /**
  * Draws udfps fingerprint if sensor isn't illuminating.
  */
-class UdfpsFpDrawable(context: Context) : UdfpsDrawable(context) {
-    override fun draw(canvas: Canvas) {
-        if (isDisplayConfigured) {
-            return
+public class UdfpsFpDrawable extends UdfpsDrawable {
+
+    UdfpsFpDrawable(@NonNull Context context) {
+        super(context);
+    }
+
+    @Override
+    public void draw(@NonNull Canvas canvas) {
+        if (isDisplayConfigured()) {
+            return;
         }
-        fingerprintDrawable.draw(canvas)
+
+        mFingerprintDrawable.draw(canvas);
     }
 }
