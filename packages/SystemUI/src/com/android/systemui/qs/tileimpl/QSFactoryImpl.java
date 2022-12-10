8a1862b65b62a1f950b90dbb93b493702fff5f0d
jhonboy121 <alfredmathew05@gmail.com>
2022-12-11 03:25:52 +0700

    [SQUASH] SystemUI: introduce refresh rate tile
    * supports cycling between 60 to peak refresh rate and auto mode
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -55,6 +55,7 @@ import com.android.systemui.qs.tiles.QRCodeScannerTile;
 import com.android.systemui.qs.tiles.QuickAccessWalletTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
+import com.android.systemui.qs.tiles.RefreshRateTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.SyncTile;
@@ -114,7 +115,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<VpnTile> mVpnTileProvider;
     private final Provider<AmbientDisplayTile> mAmbientDisplayTileProvider;
     private final Provider<RebootTile> mRebootTileProvider;
-
+    private final Provider<RefreshRateTile> mRefreshRateTileProvider;
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
 
@@ -158,6 +159,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<SyncTile> syncTileProvider,
             Provider<SoundTile> soundTileProvider,
             Provider<VpnTile> vpnTileProvider,
+            Provider<RefreshRateTile> refreshRateTileProvider,
             Provider<AmbientDisplayTile> ambientDisplayTileProvider,
             Provider<RebootTile> rebootTileProvider) {
         mQsHostLazy = qsHostLazy;
@@ -201,6 +203,7 @@ public class QSFactoryImpl implements QSFactory {
         mVpnTileProvider = vpnTileProvider;
         mAmbientDisplayTileProvider = ambientDisplayTileProvider;
         mRebootTileProvider = rebootTileProvider;
+        mRefreshRateTileProvider = refreshRateTileProvider;
     }
 
     /** Creates a tile with a type based on {@code tileSpec} */
@@ -292,6 +295,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mAmbientDisplayTileProvider.get();
             case "reboot":
                 return mRebootTileProvider.get();
+            case "refresh_rate":
+                return mRefreshRateTileProvider.get();
         }
 
         // Custom tiles
