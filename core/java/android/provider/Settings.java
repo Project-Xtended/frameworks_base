0ab00cd33844720f18fd563552a7f83fc9152ff4
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-26 05:49:14 +0800

    SystemUI: Lockscreen clock format settings [1/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7012,6 +7012,12 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Lockscreen clock format
+         * @hide
+         */
+        public static final String CLOCK_USE_CUSTOM_FORMAT = "clock_use_custom_format";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/theme/customThemeOverlayController.kt
+++ packages/SystemUI/src/com/android/systemui/theme/customThemeOverlayController.kt
@@ -51,6 +51,7 @@ import com.android.systemui.statusbar.policy.DeviceProvisionedController
 import com.android.systemui.theme.ThemeOverlayApplier
 import com.android.systemui.theme.ThemeOverlayController
 import com.android.systemui.util.settings.SecureSettings
+import com.android.systemui.util.settings.SystemSettings
 
 import dev.kdrag0n.colorkt.Color
 import dev.kdrag0n.colorkt.cam.Zcam
@@ -79,6 +80,7 @@ class customThemeOverlayController @Inject constructor(
     @Background bgExecutor: Executor,
     themeOverlayApplier: ThemeOverlayApplier,
     private val secureSettings: SecureSettings,
+    private val systemSettings: SystemSettings,
     wallpaperManager: WallpaperManager,
     userManager: UserManager,
     deviceProvisionedController: DeviceProvisionedController,
@@ -105,6 +107,7 @@ class customThemeOverlayController @Inject constructor(
     resources,
     wakefulnessLifecycle,
     configurationController,
+    systemSettings,
 ) {
 
     private val darkConfigurationListener = object : ConfigurationListener {
