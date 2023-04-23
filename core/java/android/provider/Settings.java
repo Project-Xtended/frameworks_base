1a9b17bb38aeb06205f9beb7f09877067b29aaa8
SamarV-121 <samarvispute121@pm.me>
2023-04-23 13:44:56 +0530

    SystemUI: Add brightness slider styles [1/3]
    Change-Id: I0f2c05a3d0b83ba9ddaca23e08e97e87a5c69a2c
    Signed-off-by: rayavanindra <rayavanindra@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12900,6 +12900,12 @@ public final class Settings {
         @Readable
         public static final String GAMES_DEVICE_SPOOF = "games_device_spoof";
 
+	/**
+         * Brightness slider styles
+         * @hide
+         */
+        public static final String BRIGHTNESS_SLIDER_STYLE = "brightness_slider_style";
+
         /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
